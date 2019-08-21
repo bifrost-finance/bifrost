09c24bdff391b1b890d4020c379558bb9f0c4dd5
Edwin Wang <lark930@gmail.com>
2019-08-21 19:34:40 +0800

    Create settlement module

---
+++ brml/settlement/src/mock.rs
@@ -0,0 +1,82 @@
+// Copyright 2019 Liebi Technologies.
+// This file is part of Bifrost.
+
+// Bifrost is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Bifrost is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Bifrost.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Test utilities
+
+#![cfg(test)]
+
+use srml_support::{impl_outer_origin, impl_outer_event, parameter_types};
+use substrate_primitives::{H256, Blake2Hasher};
+use sr_primitives::{Perbill, traits::{BlakeTwo256, IdentityLookup}, testing::Header};
+use super::*;
+
+impl_outer_origin! {
+	pub enum Origin for Test {}
+}
+
+#[derive(Clone, Eq, PartialEq)]
+pub struct Test;
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const MaximumBlockWeight: u32 = 1024;
+	pub const MaximumBlockLength: u32 = 2 * 1024;
+	pub const AvailableBlockRatio: Perbill = Perbill::one();
+}
+
+impl system::Trait for Test {
+	type Origin = Origin;
+	type Call = ();
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type WeightMultiplierUpdate = ();
+	type Header = Header;
+	type Event = ();
+	type BlockHashCount = BlockHashCount;
+	type MaximumBlockWeight = MaximumBlockWeight;
+	type MaximumBlockLength = MaximumBlockLength;
+	type AvailableBlockRatio = AvailableBlockRatio;
+	type Version = ();
+}
+
+impl brml_assets::Trait for Test {
+	type Event = ();
+	type Balance = u64;
+	type AssetId = u32;
+	type ClearingHandler = Settlement;
+}
+
+parameter_types! {
+	pub const SettlementPeriod: u64 = 24 * 60 * 10;
+}
+
+impl Trait for Test {
+	type SettlementId = u32;
+	type SettlementPeriod = SettlementPeriod;
+	type Duration = u128;
+}
+
+pub type Settlement = Module<Test>;
+pub type Assets = brml_assets::Module<Test>;
+pub type System = system::Module<Test>;
+
+pub fn new_test_ext() -> runtime_io::TestExternalities<Blake2Hasher> {
+	system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+}
