3a0124c21b931014e0af936d85eb797c8eec47da
Edwin Wang <lark930@gmail.com>
2020-01-17 14:42:33 +0800

    Update copyright's year

--- node/testing/src/client.rs
+++ node/testing/src/client.rs
@@ -1,4 +1,4 @@
-// Copyright 2019 Liebi Technologies.
+// Copyright 2019-2020 Liebi Technologies.
 // This file is part of Bifrost.
 
 // Bifrost is free software: you can redistribute it and/or modify
