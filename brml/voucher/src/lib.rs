371b71f70474a4ce3f65c6e153e6100b93f31f97
Dengjianping <djptux@gmail.com>
2020-04-17 11:00:43 +0800

    Adjust constant Fee and fix overflow issue on voucher module

--- brml/voucher/src/lib.rs
+++ brml/voucher/src/lib.rs
@@ -16,8 +16,8 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use core::convert::{TryFrom, TryInto};
-use frame_support::{decl_module, decl_event, decl_storage, ensure, Parameter};
+use core::convert::TryInto;
+use frame_support::{decl_module, decl_event, decl_storage, decl_error, ensure, Parameter, IterableStorageMap};
 use sp_runtime::traits::{AtLeast32Bit, Member, MaybeSerializeDeserialize, StaticLookup, Zero};
 use frame_system::{self as system, ensure_root};
 
@@ -44,6 +44,13 @@ decl_event! {
 	}
 }
 
+decl_error! {
+	pub enum Error for Module<T: Trait> {
+		/// Transferring too big balance
+		TransferringTooBigBalance,
+	}
+}
+
 decl_storage! {
 	trait Store for Module<T: Trait> as Voucher {
 		/// How much voucher you have
@@ -57,6 +64,8 @@ decl_storage! {
 
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		type Error = Error<T>;
+
 		fn deposit_event() = default;
 
 		#[weight = frame_support::weights::SimpleDispatchInfo::default()]
@@ -69,7 +78,7 @@ decl_module! {
 			ensure_root(origin)?;
 
 			let balance = <RemainingBNC<T>>::get();
-			ensure!(balance >= amount, "the balance you transfer cannot bigger than all you have.");
+			ensure!(balance >= amount, Error::<T>::TransferringTooBigBalance);
 
 			// ensure this address added into bifrost node
 			let dest = T::Lookup::lookup(dest)?;
@@ -93,6 +102,22 @@ decl_module! {
 		}
 
 		#[weight = frame_support::weights::SimpleDispatchInfo::default()]
+		fn intialize_all_voucher(origin) {
+			ensure_root(origin)?;
+
+			let total = TotalSuppliedBNC::<T>::get();
+			<RemainingBNC<T>>::mutate(|balance| {
+				*balance = total;
+			});
+
+			for (who, _) in <BalancesVoucher<T>>::iter() {
+				<BalancesVoucher<T>>::mutate(&who, |balance| {
+					*balance = 0.into();
+				});
+			}
+		}
+
+		#[weight = frame_support::weights::SimpleDispatchInfo::default()]
 		pub fn destroy_voucher(
 			origin,
 			dest: <T::Lookup as StaticLookup>::Source,
@@ -101,26 +126,31 @@ decl_module! {
 		) {
 			ensure_root(origin)?;
 
-			let balance = <RemainingBNC<T>>::get();
-			ensure!(balance >= amount, "you cannot destroy the balance that's bigger than all current user have.");
-
 			// ensure this address added into bifrost node
 			let dest = T::Lookup::lookup(dest)?;
+
+			let owner_balances = <BalancesVoucher<T>>::get(&dest);
+
+			ensure!(owner_balances >= amount, Error::<T>::TransferringTooBigBalance);
 
 			// do not send balances for a account multiple times, just for one time
 			if <BalancesVoucher<T>>::contains_key(&dest) {
 				// desotry balances for this account
-				<BalancesVoucher<T>>::mutate(&dest, |balance| {
-					*balance -= amount;
-				});
+				if owner_balances >= amount {
+					<BalancesVoucher<T>>::mutate(&dest, |balance| {
+						*balance -= amount;
+					});
+					// add back to total BNC
+					let remaining = RemainingBNC::<T>::get();
+					if remaining + amount <= TotalSuppliedBNC::<T>::get() {
+						<RemainingBNC<T>>::mutate(|balance| {
+								*balance += amount;
+						});
+					}
+				}
 			} else {
 				();
 			}
-
-			// add back to total BNC
-			<RemainingBNC<T>>::mutate(|balance| {
-					*balance += amount;
-			});
 
 			Self::deposit_event(RawEvent::DestroyedVoucher(dest, amount));
 		}
