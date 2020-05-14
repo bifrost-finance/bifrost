5ff17d69fae181b146a5e28f219dd16e66c22bd8
Dengjianping <djptux@gmail.com>
2020-05-14 15:03:55 +0800

    Bump substrate deps to fix runtime crashing on ungrading wasm

--- bin/utils/subkey/src/main.rs
+++ bin/utils/subkey/src/main.rs
@@ -744,6 +744,7 @@ fn create_extrinsic<C: Crypto>(
 			frame_system::CheckNonce::<Runtime>::from(i),
 			frame_system::CheckWeight::<Runtime>::new(),
 			pallet_transaction_payment::ChargeTransactionPayment::<Runtime>::from(f),
+			pallet_grandpa::ValidateEquivocationReport::<Runtime>::new(),
 		)
 	};
 	let raw_payload = SignedPayload::from_raw(
@@ -753,6 +754,7 @@ fn create_extrinsic<C: Crypto>(
 			VERSION.spec_version as u32,
 			genesis_hash,
 			genesis_hash,
+			(),
 			(),
 			(),
 			(),
