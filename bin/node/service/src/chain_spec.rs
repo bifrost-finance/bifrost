8424cc29fe398bd36bf266624fc4710ed789d6a1
Edwin Wang <lark930@gmail.com>
2020-11-13 14:56:19 +0800

    support rococo runtime

--- bin/node/service/src/chain_spec.rs
+++ bin/node/service/src/chain_spec.rs
@@ -35,7 +35,7 @@ use bifrost_runtime as bifrost;
 use rococo_runtime as rococo;
 use asgard::constants::currency::BNCS as ASG;
 use bifrost::constants::currency::BNCS as BNC;
-use rococo_runtime::constants::currency::BNCS as ROC;
+use rococo_runtime::constants::currency::BNCS as RCO;
 
 pub use node_primitives::{AccountId, AccountAsset, Balance, Cost, Income, Signature, TokenSymbol, ConvertPool};
 use sp_runtime::traits::{Verify};
@@ -78,8 +78,8 @@ pub fn asgard_config() -> Result<AsgardChainSpec, String> {
 	AsgardChainSpec::from_json_bytes(&include_bytes!("../res/asgard.json")[..])
 }
 
-pub fn rococo_config() -> Result<BifrostChainSpec, String> {
-	BifrostChainSpec::from_json_bytes(&include_bytes!("../res/rococo.json")[..])
+pub fn rococo_config() -> Result<RococoChainSpec, String> {
+	RococoChainSpec::from_json_bytes(&include_bytes!("../res/rococo.json")[..])
 }
 
 fn bifrost_session_keys(
@@ -276,6 +276,83 @@ fn asgard_staging_testnet_config_genesis() -> asgard::GenesisConfig {
 	)
 }
 
+fn rococo_staging_testnet_config_genesis() -> rococo::GenesisConfig {
+	// stash, controller, session-key
+	// generated with secret:
+	// for i in 1 2 3 4 ; do for j in stash controller; do subkey inspect "$secret"/fir/$j/$i; done; done
+	// and
+	// for i in 1 2 3 4 ; do for j in session; do subkey --ed25519 inspect "$secret"//fir//$j//$i; done; done
+
+	let initial_authorities: Vec<(AccountId, AccountId, BabeId, GrandpaId, ImOnlineId, AuthorityDiscoveryId)> = vec![(
+		// 5Fbsd6WXDGiLTxunqeK5BATNiocfCqu9bS1yArVjCgeBLkVy
+		hex!["9c7a2ee14e565db0c69f78c7b4cd839fbf52b607d867e9e9c5a79042898a0d12"].into(),
+		// 5EnCiV7wSHeNhjW3FSUwiJNkcc2SBkPLn5Nj93FmbLtBjQUq
+		hex!["781ead1e2fa9ccb74b44c19d29cb2a7a4b5be3972927ae98cd3877523976a276"].into(),
+		// 5EZaeQ8djPcq9pheJUhgerXQZt9YaHnMJpiHMRhwQeinqUW8
+		hex!["6e7e4eb42cbd2e0ab4cae8708ce5509580b8c04d11f6758dbf686d50fe9f9106"].unchecked_into(),
+		// 5Fb9ayurnxnaXj56CjmyQLBiadfRCqUbL2VWNbbe1nZU6wiC
+		hex!["9becad03e6dcac03cee07edebca5475314861492cdfc96a2144a67bbe9699332"].unchecked_into(),
+		// 5EZaeQ8djPcq9pheJUhgerXQZt9YaHnMJpiHMRhwQeinqUW8
+		hex!["6e7e4eb42cbd2e0ab4cae8708ce5509580b8c04d11f6758dbf686d50fe9f9106"].unchecked_into(),
+		// 5EZaeQ8djPcq9pheJUhgerXQZt9YaHnMJpiHMRhwQeinqUW8
+		hex!["6e7e4eb42cbd2e0ab4cae8708ce5509580b8c04d11f6758dbf686d50fe9f9106"].unchecked_into(),
+	),(
+		// 5ERawXCzCWkjVq3xz1W5KGNtVx2VdefvZ62Bw1FEuZW4Vny2
+		hex!["68655684472b743e456907b398d3a44c113f189e56d1bbfd55e889e295dfde78"].into(),
+		// 5Gc4vr42hH1uDZc93Nayk5G7i687bAQdHHc9unLuyeawHipF
+		hex!["c8dc79e36b29395413399edaec3e20fcca7205fb19776ed8ddb25d6f427ec40e"].into(),
+		// 5DhLtiaQd1L1LU9jaNeeu9HJkP6eyg3BwXA7iNMzKm7qqruQ
+		hex!["482dbd7297a39fa145c570552249c2ca9dd47e281f0c500c971b59c9dcdcd82e"].unchecked_into(),
+		// 5EockCXN6YkiNCDjpqqnbcqd4ad35nU4RmA1ikM4YeRN4WcE
+		hex!["7932cff431e748892fa48e10c63c17d30f80ca42e4de3921e641249cd7fa3c2f"].unchecked_into(),
+		// 5DhLtiaQd1L1LU9jaNeeu9HJkP6eyg3BwXA7iNMzKm7qqruQ
+		hex!["482dbd7297a39fa145c570552249c2ca9dd47e281f0c500c971b59c9dcdcd82e"].unchecked_into(),
+		// 5DhLtiaQd1L1LU9jaNeeu9HJkP6eyg3BwXA7iNMzKm7qqruQ
+		hex!["482dbd7297a39fa145c570552249c2ca9dd47e281f0c500c971b59c9dcdcd82e"].unchecked_into(),
+	),(
+		// 5DyVtKWPidondEu8iHZgi6Ffv9yrJJ1NDNLom3X9cTDi98qp
+		hex!["547ff0ab649283a7ae01dbc2eb73932eba2fb09075e9485ff369082a2ff38d65"].into(),
+		// 5FeD54vGVNpFX3PndHPXJ2MDakc462vBCD5mgtWRnWYCpZU9
+		hex!["9e42241d7cd91d001773b0b616d523dd80e13c6c2cab860b1234ef1b9ffc1526"].into(),
+		// 5DhKqkHRkndJu8vq7pi2Q5S3DfftWJHGxbEUNH43b46qNspH
+		hex!["482a3389a6cf42d8ed83888cfd920fec738ea30f97e44699ada7323f08c3380a"].unchecked_into(),
+		// 5E1jLYfLdUQKrFrtqoKgFrRvxM3oQPMbf6DfcsrugZZ5Bn8d
+		hex!["5633b70b80a6c8bb16270f82cca6d56b27ed7b76c8fd5af2986a25a4788ce440"].unchecked_into(),
+		// 5DhKqkHRkndJu8vq7pi2Q5S3DfftWJHGxbEUNH43b46qNspH
+		hex!["482a3389a6cf42d8ed83888cfd920fec738ea30f97e44699ada7323f08c3380a"].unchecked_into(),
+		// 5DhKqkHRkndJu8vq7pi2Q5S3DfftWJHGxbEUNH43b46qNspH
+		hex!["482a3389a6cf42d8ed83888cfd920fec738ea30f97e44699ada7323f08c3380a"].unchecked_into(),
+	),(
+		// 5HYZnKWe5FVZQ33ZRJK1rG3WaLMztxWrrNDb1JRwaHHVWyP9
+		hex!["f26cdb14b5aec7b2789fd5ca80f979cef3761897ae1f37ffb3e154cbcc1c2663"].into(),
+		// 5EPQdAQ39WQNLCRjWsCk5jErsCitHiY5ZmjfWzzbXDoAoYbn
+		hex!["66bc1e5d275da50b72b15de072a2468a5ad414919ca9054d2695767cf650012f"].into(),
+		// 5C4vDQxA8LTck2xJEy4Yg1hM9qjDt4LvTQaMo4Y8ne43aU6x
+		hex!["00299981a2b92f878baaf5dbeba5c18d4e70f2a1fcd9c61b32ea18daf38f4378"].unchecked_into(),
+		// 5DMa31Hd5u1dwoRKgC4uvqyrdK45RHv3CpwvpUC1EzuwDit4
+		hex!["3919132b851ef0fd2dae42a7e734fe547af5a6b809006100f48944d7fae8e8ef"].unchecked_into(),
+		// 5C4vDQxA8LTck2xJEy4Yg1hM9qjDt4LvTQaMo4Y8ne43aU6x
+		hex!["00299981a2b92f878baaf5dbeba5c18d4e70f2a1fcd9c61b32ea18daf38f4378"].unchecked_into(),
+		// 5C4vDQxA8LTck2xJEy4Yg1hM9qjDt4LvTQaMo4Y8ne43aU6x
+		hex!["00299981a2b92f878baaf5dbeba5c18d4e70f2a1fcd9c61b32ea18daf38f4378"].unchecked_into(),
+	)];
+
+	// generated with secret: subkey inspect "$secret"/fir
+	let root_key: AccountId = hex![
+		// 5Ff3iXP75ruzroPWRP2FYBHWnmGGBSb63857BgnzCoXNxfPo
+		"9ee5e5bdc0ec239eb164f865ecc345ce4c88e76ee002e0f7e318097347471809"
+	].into();
+
+	let endowed_accounts: Vec<AccountId> = vec![root_key.clone()];
+
+	rococo_testnet_genesis(
+		initial_authorities,
+		root_key,
+		Some(endowed_accounts),
+	)
+}
+
+
 pub fn bifrost_staging_testnet_config() -> BifrostChainSpec {
 	let boot_nodes = vec![];
 	BifrostChainSpec::from_genesis(
@@ -301,6 +378,22 @@ pub fn asgard_staging_testnet_config() -> AsgardChainSpec {
 		asgard_staging_testnet_config_genesis,
 		boot_nodes,
 		Some(TelemetryEndpoints::new(vec![(ASGARD_STAGING_TELEMETRY_URL.to_string(), 0)])
+			.expect("Staging telemetry url is valid; qed")),
+		None,
+		None,
+		Default::default(),
+	)
+}
+
+pub fn rococo_staging_testnet_config() -> RococoChainSpec {
+	let boot_nodes = vec![];
+	RococoChainSpec::from_genesis(
+		"Rococo Staging Testnet",
+		"rococo_staging_testnet",
+		ChainType::Live,
+		rococo_staging_testnet_config_genesis,
+		boot_nodes,
+		Some(TelemetryEndpoints::new(vec![(ROCOCO_STAGING_TELEMETRY_URL.to_string(), 0)])
 			.expect("Staging telemetry url is valid; qed")),
 		None,
 		None,
@@ -645,6 +738,149 @@ pub fn asgard_testnet_genesis(
 	}
 }
 
+/// Helper function to create rococo GenesisConfig for testing
+pub fn rococo_testnet_genesis(
+	initial_authorities: Vec<(
+		AccountId,
+		AccountId,
+		BabeId,
+		GrandpaId,
+		ImOnlineId,
+		AuthorityDiscoveryId,
+	)>,
+	root_key: AccountId,
+	endowed_accounts: Option<Vec<AccountId>>,
+) -> rococo::GenesisConfig {
+	let endowed_accounts: Vec<AccountId> = endowed_accounts.unwrap_or_else(testnet_accounts);
+	let num_endowed_accounts = endowed_accounts.len();
+
+	const ENDOWMENT: u128 = 1_000_000 * BNC;
+	const STASH: u128 = 100 * BNC;
+
+	rococo::GenesisConfig {
+		frame_system: Some(rococo::SystemConfig {
+			code: rococo::wasm_binary_unwrap().to_vec(),
+			changes_trie_config: Default::default(),
+		}),
+		pallet_balances: Some(rococo::BalancesConfig {
+			balances: endowed_accounts.iter().cloned()
+				.map(|k| (k, ENDOWMENT))
+				.chain(initial_authorities.iter().map(|x| (x.0.clone(), STASH)))
+				.collect(),
+		}),
+		pallet_indices: Some(rococo::IndicesConfig {
+			indices: vec![],
+		}),
+		pallet_session: Some(rococo::SessionConfig {
+			keys: initial_authorities.iter().map(|x| {
+				(x.0.clone(), x.0.clone(), rococo_session_keys(
+					x.2.clone(),
+					x.3.clone(),
+					x.4.clone(),
+					x.5.clone(),
+				))
+			}).collect::<Vec<_>>(),
+		}),
+		pallet_staking: Some(rococo::StakingConfig {
+			validator_count: initial_authorities.len() as u32 * 2,
+			minimum_validator_count: initial_authorities.len() as u32,
+			stakers: initial_authorities.iter().map(|x| {
+				(x.0.clone(), x.1.clone(), STASH, rococo::StakerStatus::Validator)
+			}).collect(),
+			invulnerables: initial_authorities.iter().map(|x| x.0.clone()).collect(),
+			slash_reward_fraction: Perbill::from_percent(10),
+			.. Default::default()
+		}),
+		pallet_democracy: Some(rococo::DemocracyConfig::default()),
+		pallet_elections_phragmen: Some(rococo::ElectionsConfig {
+			members: endowed_accounts.iter()
+				.take((num_endowed_accounts + 1) / 2)
+				.cloned()
+				.map(|member| (member, STASH))
+				.collect(),
+		}),
+		pallet_collective_Instance1: Some(rococo::CouncilConfig::default()),
+		pallet_collective_Instance2: Some(rococo::TechnicalCommitteeConfig {
+			members: endowed_accounts.iter()
+				.take((num_endowed_accounts + 1) / 2)
+				.cloned()
+				.collect(),
+			phantom: Default::default(),
+		}),
+		pallet_sudo: Some(rococo::SudoConfig {
+			key: root_key.clone(),
+		}),
+		pallet_babe: Some(rococo::BabeConfig {
+			authorities: vec![],
+		}),
+		pallet_im_online: Some(rococo::ImOnlineConfig {
+			keys: vec![],
+		}),
+		pallet_authority_discovery: Some(rococo::AuthorityDiscoveryConfig {
+			keys: vec![],
+		}),
+		pallet_grandpa: Some(rococo::GrandpaConfig {
+			authorities: vec![],
+		}),
+		pallet_membership_Instance1: Some(Default::default()),
+		pallet_treasury: Some(Default::default()),
+		pallet_society: Some(rococo::SocietyConfig {
+			members: endowed_accounts.iter()
+				.take((num_endowed_accounts + 1) / 2)
+				.cloned()
+				.collect(),
+			pot: 0,
+			max_members: 999,
+		}),
+		pallet_vesting: Some(Default::default()),
+		brml_assets: Some(rococo::AssetsConfig {
+			account_assets: vec![],
+			next_asset_id: 9u32, // start from 7, [0..6] has been reserved
+			token_details: vec![],
+			prices: vec![],
+		}),
+		brml_convert: Some(rococo::ConvertConfig {
+			convert_price: vec![
+				(TokenSymbol::DOT, DOLLARS / 100),
+				(TokenSymbol::KSM, DOLLARS / 100),
+				(TokenSymbol::EOS, DOLLARS / 100),
+				(TokenSymbol::IOST, DOLLARS / 100),
+			], // initialize convert price as token = 100 * vtoken
+			pool: vec![
+				(TokenSymbol::DOT, ConvertPool::new(1, 100)),
+				(TokenSymbol::KSM, ConvertPool::new(1, 100)),
+				(TokenSymbol::EOS, ConvertPool::new(1, 100)),
+				(TokenSymbol::IOST, ConvertPool::new(1, 100)),
+			],
+		}),
+		brml_bridge_eos: Some(rococo::BridgeEosConfig {
+			bridge_contract_account: (b"bifrostcross".to_vec(), 2),
+			notary_keys: initial_authorities.iter().map(|x| x.0.clone()).collect::<Vec<_>>(),
+			// alice and bob have the privilege to sign cross transaction
+			cross_chain_privilege: [(root_key.clone(), true)].iter().cloned().collect::<Vec<_>>(),
+			all_crosschain_privilege: Vec::new(),
+			cross_trade_eos_limit: 50 * DOLLARS, // 50 EOS as limit
+		}),
+		brml_bridge_iost: Some(rococo::BridgeIostConfig {
+			bridge_contract_account: (b"lispczz4".to_vec(), 1),
+			notary_keys: initial_authorities.iter().map(|x| x.0.clone()).collect::<Vec<_>>(),
+			// alice and bob have the privilege to sign cross transaction
+			cross_chain_privilege: [(root_key.clone(), true)].iter().cloned().collect::<Vec<_>>(),
+			all_crosschain_privilege: Vec::new(),
+		}),
+		brml_voucher: {
+			if let Some(vouchers) = initialize_all_vouchers() {
+				Some(rococo::VoucherConfig { voucher: vouchers })
+			} else {
+				None
+			}
+		},
+		// brml_swap: initialize_swap_module(root_key),
+		brml_swap: None,
+		parachain_info: Some(rococo::ParachainInfoConfig { parachain_id: 200u32.into() }),
+	}
+}
+
 fn bifrost_development_config_genesis(_wasm_binary: &[u8]) -> bifrost::GenesisConfig {
 	bifrost_testnet_genesis(
 		vec![get_authority_keys_from_seed("Alice")],
@@ -655,6 +891,14 @@ fn bifrost_development_config_genesis(_wasm_binary: &[u8]) -> bifrost::GenesisCo
 
 fn asgard_development_config_genesis(_wasm_binary: &[u8]) -> asgard::GenesisConfig {
 	asgard_testnet_genesis(
+		vec![get_authority_keys_from_seed("Alice")],
+		get_account_id_from_seed::<sr25519::Public>("Alice"),
+		None,
+	)
+}
+
+fn rococo_development_config_genesis(_wasm_binary: &[u8]) -> rococo::GenesisConfig {
+	rococo_testnet_genesis(
 		vec![get_authority_keys_from_seed("Alice")],
 		get_account_id_from_seed::<sr25519::Public>("Alice"),
 		None,
@@ -695,6 +939,23 @@ pub fn asgard_development_config() -> Result<AsgardChainSpec, String> {
 	))
 }
 
+/// Rococo development config (single validator Alice)
+pub fn rococo_development_config() -> Result<RococoChainSpec, String> {
+	let wasm_binary = rococo::WASM_BINARY.ok_or("Rococo development wasm not available")?;
+
+	Ok(RococoChainSpec::from_genesis(
+		"Development",
+		"dev",
+		ChainType::Development,
+		move || rococo_development_config_genesis(wasm_binary),
+		vec![],
+		None,
+		Some(DEFAULT_PROTOCOL_ID),
+		None,
+		Default::default(),
+	))
+}
+
 fn bifrost_local_testnet_genesis(_wasm_binary: &[u8]) -> bifrost::GenesisConfig {
 	bifrost_testnet_genesis(
 		vec![
@@ -717,13 +978,24 @@ fn asgard_local_testnet_genesis(_wasm_binary: &[u8]) -> asgard::GenesisConfig {
 	)
 }
 
+fn rococo_local_testnet_genesis(_wasm_binary: &[u8]) -> rococo::GenesisConfig {
+	rococo_testnet_genesis(
+		vec![
+			get_authority_keys_from_seed("Alice"),
+			get_authority_keys_from_seed("Bob"),
+		],
+		get_account_id_from_seed::<sr25519::Public>("Alice"),
+		None,
+	)
+}
+
 /// Bifrost local testnet config (multivalidator Alice + Bob)
 pub fn bifrost_local_testnet_config() -> Result<BifrostChainSpec, String> {
 	let wasm_binary = bifrost::WASM_BINARY.ok_or("Bifrost development wasm not available")?;
 
 	Ok(BifrostChainSpec::from_genesis(
-		"Local Testnet",
-		"local_testnet",
+		"Bifrost Local Testnet",
+		"bifrost_local_testnet",
 		ChainType::Local,
 		move || bifrost_local_testnet_genesis(wasm_binary),
 		vec![],
@@ -739,10 +1011,27 @@ pub fn asgard_local_testnet_config() -> Result<AsgardChainSpec, String> {
 	let wasm_binary = asgard::WASM_BINARY.ok_or("Asgard development wasm not available")?;
 
 	Ok(AsgardChainSpec::from_genesis(
-		"Local Testnet",
-		"local_testnet",
+		"Asgard Local Testnet",
+		"asgard_local_testnet",
 		ChainType::Local,
 		move || asgard_local_testnet_genesis(wasm_binary),
+		vec![],
+		None,
+		Some(DEFAULT_PROTOCOL_ID),
+		None,
+		Default::default(),
+	))
+}
+
+/// Rococo local testnet config (multivalidator Alice + Bob)
+pub fn rococo_local_testnet_config() -> Result<RococoChainSpec, String> {
+	let wasm_binary = rococo::WASM_BINARY.ok_or("Rococo development wasm not available")?;
+
+	Ok(RococoChainSpec::from_genesis(
+		"Rococo Local Testnet",
+		"rococo_local_testnet",
+		ChainType::Local,
+		move || rococo_local_testnet_genesis(wasm_binary),
 		vec![],
 		None,
 		Some(DEFAULT_PROTOCOL_ID),
@@ -825,6 +1114,46 @@ pub fn asgard_chainspec_config() -> AsgardChainSpec {
 		],
 		Some(TelemetryEndpoints::new(vec![(ASGARD_STAGING_TELEMETRY_URL.to_string(), 0)])
 			.expect("Asgard Testnet telemetry url is valid; qed")),
+		protocol_id,
+		properties,
+		Default::default(),
+	)
+}
+
+pub fn rococo_chainspec_config() -> RococoChainSpec {
+	let properties = {
+		let mut props = serde_json::Map::new();
+
+		props.insert(
+			"ss58Format".to_owned(),
+			serde_json::value::to_value(6u8).expect("The ss58Format cannot be convert to json value.")
+		);
+		props.insert(
+			"tokenDecimals".to_owned(),
+			serde_json::value::to_value(12u8).expect("The tokenDecimals cannot be convert to json value.")
+		);
+		props.insert(
+			"tokenSymbol".to_owned(),
+			serde_json::value::to_value("RCO".to_owned()).expect("The tokenSymbol cannot be convert to json value.")
+		);
+		Some(props)
+	};
+	let protocol_id = Some("rococo");
+
+	RococoChainSpec::from_genesis(
+		"Bifrost Rococo Parachain",
+		"rococo_testnet",
+		ChainType::Custom("Bifrost Rococo Testnet".into()),
+		rococo_staging_testnet_config_genesis,
+		vec![
+			"/dns/n1.testnet.liebi.com/tcp/30333/p2p/12D3KooWHjmfpAdrjL7EvZ7Zkk4pFmkqKDLL5JDENc7oJdeboxJJ".parse().expect("failed to parse multiaddress."),
+			"/dns/n2.testnet.liebi.com/tcp/30333/p2p/12D3KooWBMjifHHUZxbQaQZS9t5jMmTDtZbugAtJ8TG9RuX4umEY".parse().expect("failed to parse multiaddress."),
+			"/dns/n3.testnet.liebi.com/tcp/30333/p2p/12D3KooWLt3w5tadCR5Fc7ZvjciLy7iKJ2ZHq6qp4UVmUUHyCJuX".parse().expect("failed to parse multiaddress."),
+			"/dns/n4.testnet.liebi.com/tcp/30333/p2p/12D3KooWMduQkmRVzpwxJuN6MQT4ex1iP9YquzL4h5K9Ru8qMXtQ".parse().expect("failed to parse multiaddress."),
+			"/dns/n5.testnet.liebi.com/tcp/30333/p2p/12D3KooWLAHZyqMa9TQ1fR7aDRRKfWt857yFMT3k2ckK9mhYT9qR".parse().expect("failed to parse multiaddress.")
+		],
+		Some(TelemetryEndpoints::new(vec![(ROCOCO_STAGING_TELEMETRY_URL.to_string(), 0)])
+			.expect("Rococo Testnet telemetry url is valid; qed")),
 		protocol_id,
 		properties,
 		Default::default(),
