b7b85fc459801d07abe05b5cd6854bd30bb055e2
Edwin Wang <lark930@gmail.com>
2020-11-16 14:10:03 +0800

    fix compile

--- bin/node/service/src/lib.rs
+++ bin/node/service/src/lib.rs
@@ -279,6 +279,8 @@ pub fn new_full_base<RuntimeApi, Executor>(
 
 	let role = config.role.clone();
 	let force_authoring = config.force_authoring;
+	let backoff_authoring_blocks =
+		Some(sc_consensus_slots::BackoffAuthoringOnFinalizedHeadLagging::default());
 	let name = config.network.node_name.clone();
 	let enable_grandpa = !config.disable_grandpa;
 	let prometheus_registry = config.prometheus_registry().cloned();
@@ -322,6 +324,7 @@ pub fn new_full_base<RuntimeApi, Executor>(
 			sync_oracle: network.clone(),
 			inherent_data_providers: inherent_data_providers.clone(),
 			force_authoring,
+			backoff_authoring_blocks,
 			babe_link,
 			can_author_with,
 		};
