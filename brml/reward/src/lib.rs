05f1ab1b4a40d15ee16eb0b8c7abc62a39ddacb0
chw <chw@chwdeMacBook-Pro.local>
2020-11-18 15:59:12 +0800

    format code

--- brml/reward/src/lib.rs
+++ brml/reward/src/lib.rs
@@ -16,34 +16,36 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 extern crate alloc;
+
 use alloc::vec::Vec;
 use frame_support::{Parameter, decl_module, decl_storage};
 use node_primitives::{
-    RewardTrait, AssetTrait, TokenSymbol
+    RewardTrait, AssetTrait, TokenSymbol,
 };
 use sp_runtime::traits::{AtLeast32Bit, Member, MaybeSerializeDeserialize};
 use codec::{Encode, Decode};
+
 mod mock;
 mod tests;
 
 pub trait Trait: frame_system::Trait {
-    /// The units in which we record balances.
-    type Balance: Member + Parameter + AtLeast32Bit + Default + Copy + MaybeSerializeDeserialize + From<Self::BlockNumber>;
-    /// The arithmetic type of asset identifier.
-    type AssetId: Member + Parameter + AtLeast32Bit + Default + Copy + MaybeSerializeDeserialize;
-    /// The units in which we record costs.
-    type Cost: Member + Parameter + AtLeast32Bit + Default + Copy + MaybeSerializeDeserialize;
-    /// The units in which we record incomes.
-    type Income: Member + Parameter + AtLeast32Bit + Default + Copy + MaybeSerializeDeserialize;
-    /// Assets
-    type AssetTrait: AssetTrait<Self::AssetId, Self::AccountId, Self::Balance, Self::Cost, Self::Income>;
+	/// The units in which we record balances.
+	type Balance: Member + Parameter + AtLeast32Bit + Default + Copy + MaybeSerializeDeserialize + From<Self::BlockNumber>;
+	/// The arithmetic type of asset identifier.
+	type AssetId: Member + Parameter + AtLeast32Bit + Default + Copy + MaybeSerializeDeserialize;
+	/// The units in which we record costs.
+	type Cost: Member + Parameter + AtLeast32Bit + Default + Copy + MaybeSerializeDeserialize;
+	/// The units in which we record incomes.
+	type Income: Member + Parameter + AtLeast32Bit + Default + Copy + MaybeSerializeDeserialize;
+	/// Assets
+	type AssetTrait: AssetTrait<Self::AssetId, Self::AccountId, Self::Balance, Self::Cost, Self::Income>;
 }
 
 #[derive(Encode, Decode, Default, Clone)]
 #[cfg_attr(feature = "std", derive(Debug))]
-pub struct RewardRecord<AccountId, Balance>{
-    pub account_id: AccountId,
-    pub record_amount: Balance
+pub struct RewardRecord<AccountId, Balance> {
+	pub account_id: AccountId,
+	pub record_amount: Balance,
 }
 
 decl_storage! {
@@ -60,64 +62,63 @@ decl_module! {
 }
 
 impl<T: Trait> RewardTrait<T::Balance, T::AccountId> for Module<T> {
-
-    fn record_reward(vtoken_symbol: TokenSymbol, convert_amount: T::Balance, referer: T::AccountId) {
-        // ensure vtoken is exist
-        if Reward::<T>::get(vtoken_symbol).capacity() == 0 {
-            return
-        }
-        // Traverse
-        Reward::<T>::mutate(vtoken_symbol, |vec| {
-            let mut flag = true;
-            for item in vec.iter_mut() {
-                if item.account_id.eq(&referer) {
-                    item.record_amount += convert_amount;
-                    flag = false;
-                    break;
-                }
-            }
-            if flag {
-                // Create new account
-                let new_referer = RewardRecord::<T::AccountId, T::Balance>{
-                    account_id: referer,
-                    record_amount: convert_amount,
-                };
-                // Append to vec
-                vec.push(new_referer);
-            }
-            // Sort vec
-            vec.sort_by(|a, b| b.record_amount.partial_cmp(&a.record_amount).unwrap());
-        });
-    }
-
-    fn dispatch_reward(vtoken_symbol: TokenSymbol, staking_profit: T::Balance) {
-        // Obtain vec
-        let record_vec = Self::vtoken_reward(vtoken_symbol);
-        if record_vec.is_empty() {
-            return
-        }
-        // The total statistics
-        let sum: T::Balance = {
-            if record_vec.len() > 256 {
-                record_vec[..256].iter().fold(T::Balance::from(0u32), |acc, x| acc + x.record_amount)
-            } else {
-                record_vec.iter().fold(T::Balance::from(0u32), |acc, x| acc + x.record_amount)
-            }
-        };
-        // Dispatch reward
-        let mut length = record_vec.len();
-        if length > 256 {
-            length = 256
-        }
-        for i in 0..length {
-            let account_id = &record_vec[i].account_id;
-            let reward = record_vec[i].record_amount * staking_profit / sum ;
-            T::AssetTrait::asset_issue(vtoken_symbol, account_id, reward);
-        }
-        // Clear vec
-        Reward::<T>::mutate(vtoken_symbol, |vec| {
-            vec.clear();
-        });
-    }
+	fn record_reward(vtoken_symbol: TokenSymbol, convert_amount: T::Balance, referer: T::AccountId) {
+		// ensure vtoken is exist
+		if Reward::<T>::get(vtoken_symbol).capacity() == 0 {
+			return;
+		}
+		// Traverse
+		Reward::<T>::mutate(vtoken_symbol, |vec| {
+			let mut flag = true;
+			for item in vec.iter_mut() {
+				if item.account_id.eq(&referer) {
+					item.record_amount += convert_amount;
+					flag = false;
+					break;
+				}
+			}
+			if flag {
+				// Create new account
+				let new_referer = RewardRecord::<T::AccountId, T::Balance> {
+					account_id: referer,
+					record_amount: convert_amount,
+				};
+				// Append to vec
+				vec.push(new_referer);
+			}
+			// Sort vec
+			vec.sort_by(|a, b| b.record_amount.partial_cmp(&a.record_amount).unwrap());
+		});
+	}
+	
+	fn dispatch_reward(vtoken_symbol: TokenSymbol, staking_profit: T::Balance) {
+		// Obtain vec
+		let record_vec = Self::vtoken_reward(vtoken_symbol);
+		if record_vec.is_empty() {
+			return;
+		}
+		// The total statistics
+		let sum: T::Balance = {
+			if record_vec.len() > 256 {
+				record_vec[..256].iter().fold(T::Balance::from(0u32), |acc, x| acc + x.record_amount)
+			} else {
+				record_vec.iter().fold(T::Balance::from(0u32), |acc, x| acc + x.record_amount)
+			}
+		};
+		// Dispatch reward
+		let mut length = record_vec.len();
+		if length > 256 {
+			length = 256
+		}
+		for i in 0..length {
+			let account_id = &record_vec[i].account_id;
+			let reward = record_vec[i].record_amount * staking_profit / sum;
+			T::AssetTrait::asset_issue(vtoken_symbol, account_id, reward);
+		}
+		// Clear vec
+		Reward::<T>::mutate(vtoken_symbol, |vec| {
+			vec.clear();
+		});
+	}
 }
 
--- brml/reward/src/tests.rs
+++ brml/reward/src/tests.rs
@@ -28,38 +28,37 @@ fn record_reward_should_be_ok() {
 	});
 }
 
-pub fn common(){
-
+pub fn common() {
+	
 	// Ready data
 	let vdot_id = assets::Module::<Test>::next_asset_id();
 	let vtoken_symbol = TokenSymbol::from(vdot_id + 2);
 	assert_eq!(vtoken_symbol, TokenSymbol::vDOT);
 	let convert_amount = 100 as u64;
 	let (referer_one, referer_two) = (168 as u64, 100 as u64);
-
+	
 	// Add new referer
 	<crate::Module<Test>>::record_reward(vtoken_symbol, convert_amount, referer_one);
 	assert_eq!(1, crate::Module::<Test>::vtoken_reward(vtoken_symbol).len());
-
+	
 	// Increate different referer
 	<crate::Module<Test>>::record_reward(vtoken_symbol, convert_amount, referer_two);
 	assert_eq!(2, crate::Reward::<Test>::get(vtoken_symbol).len());
-
+	
 	// Append exist referer
 	<crate::Module<Test>>::record_reward(vtoken_symbol, convert_amount, referer_one);
 	assert_eq!(2, <crate::Reward<Test>>::get(vtoken_symbol).len());
-
+	
 	// Different vtoken （another table）
 	let vtoken_symbol = TokenSymbol::vEOS;
 	<crate::Module<Test>>::record_reward(vtoken_symbol, convert_amount, referer_one);
 	assert_eq!(1, <crate::Module<Test>>::vtoken_reward(vtoken_symbol).len());
-
 }
 
 #[test]
 fn dispatch_reward_is_be_ok() {
-	new_test_ext().execute_with(||{
-
+	new_test_ext().execute_with(|| {
+		
 		// Condition ready
 		common();
 		let (vtoken_symbol,
@@ -67,24 +66,23 @@ fn dispatch_reward_is_be_ok() {
 			referer_two,
 			staking_profit
 		) = (TokenSymbol::vDOT, 168 as u64, 100 as u64, 60 as u64);
-
+		
 		// The first query asset
-		let referer_one_assets = assets::Module::<Test>::account_assets((vtoken_symbol,referer_one));
+		let referer_one_assets = assets::Module::<Test>::account_assets((vtoken_symbol, referer_one));
 		assert_eq!(0, referer_one_assets.balance);
 		let referer_two_assets = assets::Module::<Test>::account_assets((vtoken_symbol, referer_two));
 		assert_eq!(0, referer_two_assets.balance);
-
+		
 		// Dispatch reward:
-		crate::Module::<Test>::dispatch_reward(vtoken_symbol,staking_profit);
-
+		crate::Module::<Test>::dispatch_reward(vtoken_symbol, staking_profit);
+		
 		// The second query asset
-		let referer_one_assets = assets::Module::<Test>::account_assets((vtoken_symbol,referer_one));
+		let referer_one_assets = assets::Module::<Test>::account_assets((vtoken_symbol, referer_one));
 		assert_ne!(0, referer_one_assets.balance);
 		let referer_two_assets = assets::Module::<Test>::account_assets((vtoken_symbol, referer_two));
 		debug_assert_ne!(0, referer_two_assets.balance);
-
+		
 		// Judge vtoken table whether be clear
 		debug_assert!(<crate::Module<Test>>::vtoken_reward(vtoken_symbol).is_empty());
-
 	});
 }
