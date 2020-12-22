1d286aea24475b1a67e39c792bbf159d59cba4dc
Edwin Wang <lark930@gmail.com>
2020-12-22 14:09:50 +0800

    sync with cumulus

--- bin/node/service/src/collator.rs
+++ bin/node/service/src/collator.rs
@@ -201,6 +201,8 @@ async fn start_node_impl<RB, RuntimeApi, Executor>(
 		);
 		let spawner = task_manager.spawn_handle();
 
+		let polkadot_backend = polkadot_full_node.backend.clone();
+
 		let params = StartCollatorParams {
 			para_id: id,
 			block_import: client.clone(),
@@ -214,6 +216,7 @@ async fn start_node_impl<RB, RuntimeApi, Executor>(
 			polkadot_full_node,
 			spawner,
 			backend,
+			polkadot_backend,
 		};
 
 		start_collator(params).await?;
