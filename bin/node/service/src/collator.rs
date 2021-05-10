cf249351b7dc39ed8d9c140903deba1521bfb228
Edwin <lark930@gmail.com>
2021-05-10 19:21:41 +0800

    fix license header (#90)

--- bin/node/service/src/collator.rs
+++ bin/node/service/src/collator.rs
@@ -1,18 +1,18 @@
-// Copyright 2019 Parity Technologies (UK) Ltd.
-// This file is part of Cumulus.
+// Copyright 2019-2021 Liebi Technologies.
+// This file is part of Bifrost.
 
-// Cumulus is free software: you can redistribute it and/or modify
+// Bifrost is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Cumulus is distributed in the hope that it will be useful,
+// Bifrost is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
+// along with Bifrost.  If not, see <http://www.gnu.org/licenses/>.
 
 use std::sync::Arc;
 
