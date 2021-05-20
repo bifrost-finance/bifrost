69c1520d62e0a1e2a66061f608ffcfc1ba8872f0
Allen Pocket <AllenPocketGamer@Gmail.com>
2021-05-19 18:36:22 -0700

    :white_check_mark: ($BRML) Impl tests which dont need init genesis state

--- brml/vsbond-auction/src/tests.rs
+++ brml/vsbond-auction/src/tests.rs
@@ -3,74 +3,129 @@
 use crate::mock::*;
 use crate::*;
 use frame_support::{assert_noop, assert_ok, dispatch::DispatchError};
-use node_primitives::{AssetId, Balance};
+
+fn storages_initialization() {}
+
+fn create_order_for_test(
+	owner: AccountIdOf<Test>,
+	currency_sold: CurrencyIdOf<Test>,
+	amount_sold: BalanceOf<Test>,
+	currency_expected: CurrencyIdOf<Test>,
+	amount_expected: BalanceOf<Test>,
+	order_state: OrderState,
+) -> OrderId {
+	let order_id = Pallet::<Test>::next_order_id();
+	let order_info = OrderInfo {
+		owner: owner.clone(),
+		currency_sold,
+		amount_sold,
+		currency_expected,
+		amount_expected,
+		order_id,
+		order_state,
+	};
+
+	TotalOrders::<Test>::insert(order_id, order_info);
+	SellerOrders::<Test>::mutate(owner, currency_sold, |order_ids| order_ids.push(order_id));
+
+	order_id
+}
 
 #[test]
 fn create_order_should_work() {
+	// TODO: Need Init Genesis State
 	todo!()
 }
 
 #[test]
 fn create_order_by_origin_illegal_should_fail() {
-	new_test_ext().execute_with(|| {
-		let origin_signed = Origin::signed(1);
-		let currency_sold: AssetId = 1;
-		let amount_sold: Balance = 100;
-		let currency_expected: AssetId = 2;
-		let amount_expected: Balance = 100;
-
-		assert_ok!(VSBondAuction::create_order(
-			origin_signed,
-			currency_sold,
-			amount_sold,
-			currency_expected,
-			amount_expected,
-		));
-	});
+	// TODO: Need Init Genesis State
+	todo!()
 }
 
 #[test]
 fn create_order_without_enough_currency_should_fail() {
-	new_test_ext().execute_with(|| {
-		let origin_signed = Origin::signed(1);
-		let currency_sold: AssetId = 1;
-		let amount_sold_more_than_owned: Balance = 1000;
-		let currency_expected: AssetId = 2;
-		let amount_expected: Balance = 100;
+	// TODO: Need Init Genesis State
+	todo!()
 
+}
+
+#[test]
+fn revoke_order_should_work() {
+	new_test_ext().execute_with(|| {
+		let order_id = create_order_for_test(1, 1, 100, 2, 100, OrderState::InTrade);
+		assert_ok!(VSBondAuction::revoke_order(Origin::signed(1), order_id));
+	});
+}
+
+#[test]
+fn revoke_order_not_exist_should_fail() {
+	new_test_ext().execute_with(|| {
+		let order_id = create_order_for_test(1, 1, 100, 2, 100, OrderState::InTrade);
 		assert_noop!(
-			VSBondAuction::create_order(
-				origin_signed,
-				currency_sold,
-				amount_sold_more_than_owned,
-				currency_expected,
-				amount_expected,
-			),
-			Error::<Test>::NotEnoughCurrencySold
+			VSBondAuction::revoke_order(Origin::signed(1), order_id + 1),
+			Error::<Test>::NotFindOrderInfo
 		);
 	});
 }
 
 #[test]
-fn revoke_order_should_work() {}
+fn revoke_order_by_origin_illegal_should_fail() {
+	new_test_ext().execute_with(|| {
+		let order_id = create_order_for_test(1, 1, 100, 2, 100, OrderState::InTrade);
+
+		assert_noop!(
+			VSBondAuction::revoke_order(Origin::root(), order_id),
+			DispatchError::BadOrigin,
+		);
+		assert_noop!(
+			VSBondAuction::revoke_order(Origin::signed(2), order_id),
+			Error::<Test>::ForbidRevokeOrderWithoutOwnership,
+		);
+		assert_noop!(
+			VSBondAuction::revoke_order(Origin::none(), order_id),
+			DispatchError::BadOrigin,
+		);
+	});
+}
 
 #[test]
-fn revoke_order_not_exist_should_fail() {}
+fn revoke_order_not_in_trade_should_fail() {
+	new_test_ext().execute_with(|| {
+		let order_id_revoked = create_order_for_test(1, 1, 100, 2, 100, OrderState::Revoked);
+		let order_id_clinchd = create_order_for_test(1, 1, 100, 2, 100, OrderState::Clinchd);
+
+		assert_noop!(
+			VSBondAuction::revoke_order(Origin::signed(1), order_id_revoked),
+			Error::<Test>::ForbidRevokeOrderNotInTrade,
+		);
+		assert_noop!(
+			VSBondAuction::revoke_order(Origin::signed(1), order_id_clinchd),
+			Error::<Test>::ForbidRevokeOrderNotInTrade,
+		);
+	});
+}
 
 #[test]
-fn revoke_order_by_origin_illegal_should_fail() {}
+fn clinch_order_should_work() {
+	// TODO: Need Init Genesis State
+	todo!()
+}
 
 #[test]
-fn revoke_order_not_in_trade_should_fail() {}
+fn clinch_order_not_exist_should_fail() {
+	// TODO: Need Init Genesis State
+	todo!()
+}
 
 #[test]
-fn clinch_order_should_work() {}
+fn clinck_order_by_origin_illegal_should_fail() {
+	// TODO: Need Init Genesis State
+	todo!()
+}
 
 #[test]
-fn clinch_order_not_exist_should_fail() {}
-
-#[test]
-fn clinck_order_by_origin_illegal_should_fail() {}
-
-#[test]
-fn clinch_order_not_in_trade_should_fail() {}
+fn clinch_order_not_in_trade_should_fail() {
+	// TODO: Need Init Genesis State
+	todo!()
+}
