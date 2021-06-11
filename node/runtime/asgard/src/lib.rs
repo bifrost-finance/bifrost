07ae91fadf39facf33ddba242ae49bcfeefec20e
ron.yang <yrong1997@gmail.com>
2021-06-11 14:08:33 +0800

    Upgrade xcm&minor fix

--- node/runtime/asgard/src/lib.rs
+++ node/runtime/asgard/src/lib.rs
@@ -492,7 +492,7 @@ impl Config for XcmConfig {
 
 /// No local origins on this chain are allowed to dispatch XCM sends/executions.
 pub type LocalOriginToLocation = (
-	SignedToAccountId32<Origin, AccountId, RococoNetwork>,
+	SignedToAccountId32<Origin, AccountId, AnyNetwork>,
 );
 
 /// The means for routing XCM messages which are not for local execution into the right message
