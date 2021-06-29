25830e211c6e1a141a821dfbc8f5d02fd4745543
yrong1997 <yrong1997@gmail.com>
2021-06-29 21:49:52 +0800

    Add fmt hook (#155)
    * Add fmt hook
    
    * install rustfmt

--- scripts/init.sh
+++ scripts/init.sh
@@ -11,6 +11,8 @@ fi
 
 rustup target add wasm32-unknown-unknown --toolchain nightly
 
+rustup component add rustfmt
+
 # Install wasm-gc. It's useful for stripping slimming down wasm binaries.
 command -v wasm-gc || \
 	cargo +nightly install --git https://github.com/alexcrichton/wasm-gc --force
