453cd5874715587cbecbe53ccda20ed306114b37
Allen Pocket <AllenPocketGamer@Gmail.com>
2021-06-30 06:40:22 -0700

    :bug: ($PALLET) Fix salp mock error

--- pallets/salp/src/mock.rs
+++ pallets/salp/src/mock.rs
@@ -15,6 +15,7 @@ use xcm::{
 	DoubleEncoded,
 };
 use xcm_builder::{EnsureXcmOrigin, SignedToAccountId32};
+use crate as salp;
 
 pub const BNCS: Balance = 1_000_000_000_000;
 pub const DOLLARS: Balance = BNCS;
@@ -42,7 +43,7 @@ construct_runtime!(
 		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
 		Tokens: orml_tokens::{Pallet, Call, Storage, Event<T>},
 		Bancor: bifrost_bancor::{Pallet, Call, Config<T>, Storage, Event<T>},
-		Salp: crate::{Pallet, Call, Storage, Event<T>},
+		Salp: salp::{Pallet, Call, Storage, Event<T>},
 	}
 );
 
@@ -130,7 +131,7 @@ parameter_types! {
 
 type LocalOriginToLocation = (SignedToAccountId32<Origin, AccountId, AnyNetwork>,);
 
-impl crate::Config for Test {
+impl salp::Config for Test {
 	type BancorPool = Bancor;
 	type BifrostXcmExecutor = MockXcmExecutor;
 	type Event = Event;
