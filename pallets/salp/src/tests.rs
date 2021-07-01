4db57d698a57af5d5fac859b476ea4a39a378bb7
Allen Pocket <AllenPocketGamer@Gmail.com>
2021-06-30 19:53:43 -0700

    :white_check_mark: ($PALLET) Add some unit-tests for salp

--- pallets/salp/src/tests.rs
+++ pallets/salp/src/tests.rs
@@ -1,23 +1,74 @@
 #![cfg(test)]
 
+use frame_support::{assert_noop, assert_ok, dispatch::DispatchError};
+
+use crate as salp;
 use crate::mock::*;
 
 #[test]
 fn create_fund_should_work() {
-	todo!()
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1,));
+		assert_ok!(Salp::funds(3_000).ok_or(()));
+		assert_eq!(Salp::current_trie_index(), 1);
+	});
 }
 
 #[test]
-fn create_order_under_non_signed_should_fail() {
-	todo!()
+fn create_fund_under_non_signed_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_noop!(
+			Salp::create(Origin::root(), 3_000, 1_000, 0, SlotLength::get() - 1,),
+			DispatchError::BadOrigin,
+		);
+
+		assert_noop!(
+			Salp::create(Origin::none(), 3_000, 1_000, 0, SlotLength::get() - 1,),
+			DispatchError::BadOrigin,
+		);
+	});
 }
 
 #[test]
 fn create_fund_existed_should_fail() {
-	todo!()
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1,),);
+
+		assert_noop!(
+			Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1,),
+			salp::Error::<Test>::FundExisted,
+		);
+	});
+}
+
+#[test]
+fn create_fund_exceed_slot_limit_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_noop!(
+			Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get()),
+			salp::Error::<Test>::LastSlotTooFarInFuture,
+		);
+	});
 }
 
 #[test]
 fn create_fund_first_slot_bigger_than_last_slot_should_fail() {
-	todo!()
+	new_test_ext().execute_with(|| {
+		assert_noop!(
+			Salp::create(Some(ALICE).into(), 3_000, 1_000, SlotLength::get() - 1, 0),
+			salp::Error::<Test>::LastSlotBeforeFirstSlot,
+		);
+	});
+}
+
+// Utilities Test
+
+#[test]
+fn check_next_trie_index() {
+	new_test_ext().execute_with(|| {
+		for i in 0 .. 100 {
+			assert_eq!(Salp::current_trie_index(), i);
+			assert_ok!(Salp::next_trie_index());
+		}
+	});
 }
