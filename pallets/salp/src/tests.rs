a55fc7e69f6f18ed7e45d34515228a0303ac50ff
Allen Pocket <AllenPocketGamer@Gmail.com>
2021-07-01 01:16:37 -0700

    :white_check_mark: ($PALLET) Add some new unit-tests

--- pallets/salp/src/tests.rs
+++ pallets/salp/src/tests.rs
@@ -22,8 +22,7 @@
 
 use frame_support::{assert_noop, assert_ok, dispatch::DispatchError};
 
-use crate as salp;
-use crate::mock::*;
+use crate::{mock::*, Error, FundStatus};
 
 #[test]
 fn create_fund_should_work() {
@@ -56,7 +55,7 @@ fn create_fund_existed_should_fail() {
 
 		assert_noop!(
 			Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1,),
-			salp::Error::<Test>::FundExisted,
+			Error::<Test>::FundExisted,
 		);
 	});
 }
@@ -66,7 +65,7 @@ fn create_fund_exceed_slot_limit_should_fail() {
 	new_test_ext().execute_with(|| {
 		assert_noop!(
 			Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get()),
-			salp::Error::<Test>::LastSlotTooFarInFuture,
+			Error::<Test>::LastSlotTooFarInFuture,
 		);
 	});
 }
@@ -76,8 +75,187 @@ fn create_fund_first_slot_bigger_than_last_slot_should_fail() {
 	new_test_ext().execute_with(|| {
 		assert_noop!(
 			Salp::create(Some(ALICE).into(), 3_000, 1_000, SlotLength::get() - 1, 0),
-			salp::Error::<Test>::LastSlotBeforeFirstSlot,
+			Error::<Test>::LastSlotBeforeFirstSlot,
 		);
+	});
+}
+
+#[test]
+fn set_fund_success_should_work() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_ok!(Salp::fund_success(Some(ALICE).into(), 3_000));
+
+		// Check status
+		let fund = Salp::funds(3_000).unwrap();
+		assert_eq!(fund.status, FundStatus::Success);
+	});
+}
+
+#[test]
+fn set_fund_success_with_wrong_origin_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_noop!(Salp::fund_success(Origin::root(), 3_000), Error::<Test>::InvalidOrigin);
+		assert_noop!(Salp::fund_success(Origin::none(), 3_000), Error::<Test>::InvalidOrigin);
+		assert_noop!(Salp::fund_success(Some(BRUCE).into(), 3_000), Error::<Test>::InvalidOrigin);
+	})
+}
+
+#[test]
+fn set_fund_success_with_wrong_para_id_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_noop!(Salp::fund_success(Some(ALICE).into(), 4_000), Error::<Test>::InvalidParaId);
+	});
+}
+
+#[test]
+fn set_fund_success_with_wrong_fund_status_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_ok!(Salp::fund_fail(Some(ALICE).into(), 3_000));
+		assert_noop!(
+			Salp::fund_success(Some(ALICE).into(), 3_000),
+			Error::<Test>::InvalidFundStatus
+		);
+	});
+}
+
+#[test]
+fn set_fund_fail_should_work() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_ok!(Salp::fund_fail(Some(ALICE).into(), 3_000));
+
+		// Check status
+		let fund = Salp::funds(3_000).unwrap();
+		assert_eq!(fund.status, FundStatus::Failed);
+	});
+}
+
+#[test]
+fn set_fund_fail_with_wrong_origin_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_noop!(Salp::fund_fail(Origin::root(), 3_000), Error::<Test>::InvalidOrigin);
+		assert_noop!(Salp::fund_fail(Origin::none(), 3_000), Error::<Test>::InvalidOrigin);
+		assert_noop!(Salp::fund_fail(Some(BRUCE).into(), 3_000), Error::<Test>::InvalidOrigin);
+	});
+}
+
+#[test]
+fn set_fund_fail_with_wrong_para_id_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_noop!(Salp::fund_fail(Some(ALICE).into(), 4_000), Error::<Test>::InvalidParaId);
+	});
+}
+
+#[test]
+fn set_fund_fail_with_wrong_fund_status_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_ok!(Salp::fund_success(Some(ALICE).into(), 3_000));
+		assert_noop!(Salp::fund_fail(Some(ALICE).into(), 3_000), Error::<Test>::InvalidFundStatus);
+	});
+}
+
+#[test]
+fn set_fund_retire_should_work() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_ok!(Salp::fund_success(Some(ALICE).into(), 3_000));
+		assert_ok!(Salp::fund_retire(Some(ALICE).into(), 3_000));
+
+		// Check status
+		let fund = Salp::funds(3_000).unwrap();
+		assert_eq!(fund.status, FundStatus::Retired);
+	});
+}
+
+#[test]
+fn set_fund_retire_with_wrong_origin_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_ok!(Salp::fund_success(Some(ALICE).into(), 3_000));
+		assert_noop!(Salp::fund_retire(Origin::root(), 3_000), Error::<Test>::InvalidOrigin);
+		assert_noop!(Salp::fund_retire(Origin::none(), 3_000), Error::<Test>::InvalidOrigin);
+		assert_noop!(Salp::fund_retire(Some(BRUCE).into(), 3_000), Error::<Test>::InvalidOrigin);
+	});
+}
+
+#[test]
+fn set_fund_retire_with_wrong_para_id_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_ok!(Salp::fund_success(Some(ALICE).into(), 3_000));
+		assert_noop!(Salp::fund_retire(Some(ALICE).into(), 4_000), Error::<Test>::InvalidParaId);
+	});
+}
+
+#[test]
+fn set_fund_retire_with_with_wrong_fund_status_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_noop!(
+			Salp::fund_retire(Some(ALICE).into(), 3_000),
+			Error::<Test>::InvalidFundStatus
+		);
+	});
+}
+
+#[test]
+fn set_fund_end_should_work() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_ok!(Salp::fund_success(Some(ALICE).into(), 3_000));
+		assert_ok!(Salp::fund_retire(Some(ALICE).into(), 3_000));
+		assert_ok!(Salp::withdraw(Some(ALICE).into(), 3_000));
+		assert_ok!(Salp::confirm_withdraw(Some(ALICE).into(), 3_000, true));
+		assert_ok!(Salp::fund_end(Some(ALICE).into(), 3_000));
+
+		// Check storage
+		let fund = Salp::funds(3_000).unwrap();
+		assert_eq!(fund.status, FundStatus::End);
+	});
+}
+
+#[test]
+fn set_fund_end_with_wrong_origin_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_ok!(Salp::fund_success(Some(ALICE).into(), 3_000));
+		assert_ok!(Salp::fund_retire(Some(ALICE).into(), 3_000));
+		assert_ok!(Salp::withdraw(Some(ALICE).into(), 3_000));
+		assert_ok!(Salp::confirm_withdraw(Some(ALICE).into(), 3_000, true));
+
+		assert_noop!(Salp::fund_end(Origin::root(), 3_000), Error::<Test>::InvalidOrigin);
+		assert_noop!(Salp::fund_end(Origin::none(), 3_000), Error::<Test>::InvalidOrigin);
+		assert_noop!(Salp::fund_end(Some(BRUCE).into(), 3_000), Error::<Test>::InvalidOrigin);
+	});
+}
+
+#[test]
+fn set_fund_end_with_wrong_wrong_para_id_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_ok!(Salp::fund_success(Some(ALICE).into(), 3_000));
+		assert_ok!(Salp::fund_retire(Some(ALICE).into(), 3_000));
+		assert_ok!(Salp::withdraw(Some(ALICE).into(), 3_000));
+		assert_ok!(Salp::confirm_withdraw(Some(ALICE).into(), 3_000, true));
+
+		assert_noop!(Salp::fund_end(Some(ALICE).into(), 4_000), Error::<Test>::InvalidParaId);
+	});
+}
+
+#[test]
+fn set_fund_end_with_wrong_fund_status_should_fail() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Salp::create(Some(ALICE).into(), 3_000, 1_000, 0, SlotLength::get() - 1));
+		assert_ok!(Salp::fund_success(Some(ALICE).into(), 3_000));
+
+		assert_noop!(Salp::fund_end(Some(ALICE).into(), 3_000), Error::<Test>::InvalidFundStatus);
 	});
 }
 
@@ -90,7 +268,6 @@ fn contribute_under_non_signed_should_fail() {}
 #[test]
 fn contribute_to_non_exist_parachain_should_fail() {}
 
-#[test]
 // Utilities Test
 #[test]
 fn check_next_trie_index() {
