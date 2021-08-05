0a9b363d6deb3d395b1c90b99a6b9b163f2da7dd
Herry Ho <45537372+herryho@users.noreply.github.com>
2021-08-05 13:58:46 +0800

    - fix bancor missing 2 rpc interfaces (#207)
    * - fix bancor missing 2 rpc interfaces
    - fix the benchmarking bugs caused by asgard-dev runtime being changed to dev runtime
    
    * Fix benchmark build
    
    Co-authored-by: ron.yang <yrong1997@gmail.com>

--- scripts/run_all_benches.sh
+++ scripts/run_all_benches.sh
@@ -9,11 +9,11 @@ runtimes=(
 
 # cargo build --locked --release
 for runtime in "${runtimes[@]}"; do
-  cargo run --release --features="runtime-benchmarks,with-${runtime}-runtime" --locked benchmark --chain "${runtime}-dev" --execution=wasm --wasm-execution=compiled --pallet "*" --extrinsic "*" --repeat 0 | sed -r -e 's/Pallet: "([a-z_:]+)".*/\1/' | uniq | grep -v frame_system > "${runtime}_pallets"
+  cargo run --release --features="runtime-benchmarks,with-${runtime}-runtime" --locked benchmark --chain "${runtime}-local" --execution=wasm --wasm-execution=compiled --pallet "*" --extrinsic "*" --repeat 0 | sed -r -e 's/Pallet: "([a-z_:]+)".*/\1/' | uniq | grep -v frame_system > "${runtime}_pallets"
   while read -r line; do
     pallet="$(echo "$line" | cut -d' ' -f1)";
     echo "Runtime: $runtime. Pallet: $pallet";
-    cargo run --release --features="runtime-benchmarks,with-${runtime}-runtime" -- benchmark --chain="${runtime}-dev" --steps=50 --repeat=20 --pallet="$pallet" --extrinsic="*" --execution=wasm --wasm-execution=compiled --heap-pages=4096 --header=./HEADER-GPL3 --output="./runtime/${runtime}/src/weights/${pallet/::/_}.rs"
+    cargo run --release --features="runtime-benchmarks,with-${runtime}-runtime" -- benchmark --chain="${runtime}-local" --steps=50 --repeat=20 --pallet="$pallet" --extrinsic="*" --execution=wasm --wasm-execution=compiled --heap-pages=4096 --header=./HEADER-GPL3 --output="./runtime/${runtime}/src/weights/${pallet/::/_}.rs"
   done < "${runtime}_pallets"
   rm "${runtime}_pallets"
 done
