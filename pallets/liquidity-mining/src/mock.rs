39296e33ecba53c463cf25f8ea2f5ae4e1a820e5
Allen Pocket <AllenPocketGamer@Gmail.com>
2021-08-12 18:34:51 +0800

    :white_check_mark: ($PALLET) Impl mock

--- pallets/liquidity-mining/src/mock.rs
+++ pallets/liquidity-mining/src/mock.rs
@@ -15,3 +15,173 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use frame_support::{
+	construct_runtime, parameter_types,
+	sp_runtime::{
+		generic,
+		traits::{BlakeTwo256, IdentifyAccount, IdentityLookup, Verify},
+		MultiSignature,
+	},
+};
+use node_primitives::{Amount, Balance, CurrencyId, TokenSymbol};
+use sp_core::H256;
+
+use crate as lm;
+
+pub(crate) type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
+pub(crate) type Block = frame_system::mocking::MockBlock<T>;
+pub(crate) type BlockNumber = u32;
+pub(crate) type Index = u32;
+pub(crate) type Signature = MultiSignature;
+pub(crate) type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<T>;
+
+construct_runtime!(
+	pub enum T where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+		Currencies: orml_currencies::{Pallet, Call, Event<T>},
+		Tokens: orml_tokens::{Pallet, Call, Storage, Event<T>},
+		TechnicalCommittee: pallet_collective::<Instance1>::{Pallet, Call, Storage, Origin<T>, Event<T>, Config<T>},
+		LiquidityMining: lm::{Pallet, Call, Storage, Event<T>},
+	}
+);
+
+parameter_types! {
+	pub const NativeCurrencyId: CurrencyId = CurrencyId::Native(TokenSymbol::ASG);
+	pub const RelayCurrencyId: CurrencyId = CurrencyId::Token(TokenSymbol::KSM);
+	pub const StableCurrencyId: CurrencyId = CurrencyId::Stable(TokenSymbol::AUSD);
+}
+
+parameter_types! {
+	pub const BlockHashCount: BlockNumber = 250;
+	pub BlockWeights: frame_system::limits::BlockWeights =
+		frame_system::limits::BlockWeights::simple_max(1024);
+}
+
+impl frame_system::Config for T {
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type AccountId = AccountId;
+	type BaseCallFilter = ();
+	type BlockHashCount = BlockHashCount;
+	type BlockLength = ();
+	type BlockNumber = BlockNumber;
+	type BlockWeights = ();
+	type Call = Call;
+	type DbWeight = ();
+	type Event = Event;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type Header = generic::Header<BlockNumber, BlakeTwo256>;
+	type Index = Index;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type OnKilledAccount = ();
+	type OnNewAccount = ();
+	type OnSetCode = ();
+	type Origin = Origin;
+	type PalletInfo = PalletInfo;
+	type SS58Prefix = ();
+	type SystemWeightInfo = ();
+	type Version = ();
+}
+
+parameter_types! {
+	pub const ExistentialDeposit: u128 = 0;
+	pub const TransferFee: u128 = 0;
+	pub const CreationFee: u128 = 0;
+	pub const TransactionByteFee: u128 = 0;
+	pub const MaxLocks: u32 = 999_999;
+	pub const MaxReserves: u32 = 999_999;
+}
+
+impl pallet_balances::Config for T {
+	type AccountStore = System;
+	/// The type for recording an account's balance.
+	type Balance = Balance;
+	type DustRemoval = ();
+	/// The ubiquitous event type.
+	type Event = Event;
+	type ExistentialDeposit = ExistentialDeposit;
+	type MaxLocks = MaxLocks;
+	type MaxReserves = MaxReserves;
+	type ReserveIdentifier = [u8; 8];
+	type WeightInfo = pallet_balances::weights::SubstrateWeight<T>;
+}
+
+pub type BifrostToken = orml_currencies::BasicCurrencyAdapter<T, Balances, Amount, BlockNumber>;
+
+impl orml_currencies::Config for T {
+	type Event = Event;
+	type GetNativeCurrencyId = NativeCurrencyId;
+	type MultiCurrency = Tokens;
+	type NativeCurrency = BifrostToken;
+	type WeightInfo = ();
+}
+
+orml_traits::parameter_type_with_key! {
+	pub ExistentialDeposits: |_currency_id: CurrencyId| -> Balance {
+		0
+	};
+}
+
+impl orml_tokens::Config for T {
+	type Amount = Amount;
+	type Balance = Balance;
+	type CurrencyId = CurrencyId;
+	type Event = Event;
+	type ExistentialDeposits = ExistentialDeposits;
+	type MaxLocks = MaxLocks;
+	type OnDust = ();
+	type WeightInfo = ();
+}
+
+parameter_types! {
+	pub const TechnicalMotionDuration: BlockNumber = 3 * DAYS;
+	pub const TechnicalMaxProposals: u32 = 100;
+	pub const TechnicalMaxMembers: u32 = 100;
+}
+
+type TechnicalCollective = pallet_collective::Instance1;
+impl pallet_collective::Config<TechnicalCollective> for T {
+	type DefaultVote = pallet_collective::PrimeDefaultVote;
+	type Event = Event;
+	type MaxMembers = TechnicalMaxMembers;
+	type MaxProposals = TechnicalMaxProposals;
+	type MotionDuration = TechnicalMotionDuration;
+	type Origin = Origin;
+	type Proposal = Call;
+	type WeightInfo = pallet_collective::weights::SubstrateWeight<T>;
+}
+
+parameter_types! {
+	pub const MaximumDepositInPool: Balance = MILLION_UNIT;
+	pub const MinimumDeposit: Balance = MICRO_UNIT;
+	pub const MinimumRewardPerBlock: Balance = NANO_UNIT;
+	pub const MinimumDuration: BlockNumber = MINUTES;
+	pub const MaximumActivated: u32 = 4;
+}
+
+impl lm::Config for T {
+	type Event = Event;
+	type ControlOrigin = pallet_collective::EnsureMember<AccountId, TechnicalCollective>;
+	type MultiCurrency = Tokens;
+	type RelayChainToken = RelayCurrencyId;
+	type MaximumDepositInPool = MaximumDepositInPool;
+	type MinimumDeposit = MinimumDeposit;
+	type MinimumRewardPerBlock = MinimumRewardPerBlock;
+	type MinimumDuration = MinimumDuration;
+	type MaximumActivated = MaximumActivated;
+}
+
+pub const MINUTES: BlockNumber = 60 / (12 as BlockNumber);
+pub const HOURS: BlockNumber = MINUTES * 60;
+pub const DAYS: BlockNumber = HOURS * 24;
+
+pub const NANO_UNIT: Balance = 1_000;
+pub const MICRO_UNIT: Balance = 1_000_000;
+pub const UNIT: Balance = 1_000_000_000_000;
+pub const MILLION_UNIT: Balance = 1_000_000 * UNIT;
