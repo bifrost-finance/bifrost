049006388341bf400f6e672157c66afb209279d2
Edwin Wang <lark930@gmail.com>
2021-08-17 23:18:52 +0800

    bump spec_version

--- runtime/bifrost/src/lib.rs
+++ runtime/bifrost/src/lib.rs
@@ -95,7 +95,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bifrost"),
 	impl_name: create_runtime_str!("bifrost"),
 	authoring_version: 1,
-	spec_version: 801,
+	spec_version: 802,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
