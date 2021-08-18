4b9e4b256a40ca0b12b26665634864b225921e9c
Edwin Wang <lark930@gmail.com>
2021-08-18 14:11:29 +0800

    add vesting into benchmarks

--- runtime/bifrost/src/lib.rs
+++ runtime/bifrost/src/lib.rs
@@ -840,6 +840,7 @@ impl_runtime_apis! {
 			add_benchmark!(params, batches, pallet_timestamp, Timestamp);
 			// add_benchmark!(params, batches, pallet_treasury, Treasury);
 			add_benchmark!(params, batches, pallet_utility, Utility);
+			add_benchmark!(params, batches, pallet_vesting, Vesting);
 
 			if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
 			Ok(batches)
