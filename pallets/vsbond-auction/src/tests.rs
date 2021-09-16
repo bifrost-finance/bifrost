485a150d549fdc34fc09ec71be5201706c8d14fc
Allen Pocket <AllenPocketGamer@Gmail.com>
2021-09-16 11:19:38 +0800

    :white_check_mark: ($PALLET) Add unit-tests

--- pallets/vsbond-auction/src/tests.rs
+++ pallets/vsbond-auction/src/tests.rs
@@ -246,40 +246,82 @@ fn revoke_order_should_work() {
 }
 
 #[test]
-fn revoke_order_which_be_partial_clinchd_should_work() {
+fn revoke_sell_order_which_be_partial_clinchd_should_work() {
 	new_test_ext().execute_with(|| {
 		assert_ok!(Auction::create_order(
 			Some(ALICE).into(),
 			3000,
 			13,
 			20,
-			50,
-			50,
+			100,
+			33,
 			OrderType::Sell
 		));
-		assert_ok!(Auction::partial_clinch_order(Some(BRUCE).into(), 0, 25));
+		assert_ok!(Auction::partial_clinch_order(Some(BRUCE).into(), 0, 33));
 		assert_ok!(Auction::revoke_order(Some(ALICE).into(), 0));
-		assert_ok!(Auction::create_order(Some(ALICE).into(), 3000, 13, 20, 50, 50, OrderType::Buy));
-		assert_ok!(Auction::partial_clinch_order(Some(BRUCE).into(), 1, 25));
-		assert_ok!(Auction::revoke_order(Some(ALICE).into(), 1));
 
-		assert_eq!(Auction::order_id(), 2);
+		assert_eq!(Auction::order_id(), 1);
 
 		let user_sell_order_ids = Auction::user_order_ids(ALICE, OrderType::Sell);
-		let user_buy_order_ids = Auction::user_order_ids(ALICE, OrderType::Buy);
 		assert_eq!(user_sell_order_ids.len(), 0);
-		assert_eq!(user_buy_order_ids.len(), 0);
 
 		assert!(Auction::order_info(0).is_none());
-		assert!(Auction::order_info(1).is_none());
 
-		assert_eq!(Tokens::accounts(ALICE, VSBOND).free, 100);
+		assert_eq!(Tokens::accounts(ALICE, VSBOND).free, 67);
 		assert_eq!(Tokens::accounts(ALICE, VSBOND).frozen, 0);
 		assert_eq!(Tokens::accounts(ALICE, VSBOND).reserved, 0);
 
-		assert_eq!(Tokens::accounts(ALICE, TOKEN).free, 100);
+		assert_eq!(Tokens::accounts(ALICE, TOKEN).free, 110);
 		assert_eq!(Tokens::accounts(ALICE, TOKEN).frozen, 0);
 		assert_eq!(Tokens::accounts(ALICE, TOKEN).reserved, 0);
+
+		assert_eq!(Tokens::accounts(BRUCE, VSBOND).free, 133);
+		assert_eq!(Tokens::accounts(BRUCE, VSBOND).frozen, 0);
+		assert_eq!(Tokens::accounts(BRUCE, VSBOND).reserved, 0);
+
+		assert_eq!(Tokens::accounts(BRUCE, TOKEN).free, 90);
+		assert_eq!(Tokens::accounts(BRUCE, TOKEN).frozen, 0);
+		assert_eq!(Tokens::accounts(BRUCE, TOKEN).reserved, 0);
+	});
+}
+
+#[test]
+fn revoke_buy_order_which_be_partial_clinchd_should_work() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Auction::create_order(
+			Some(ALICE).into(),
+			3000,
+			13,
+			20,
+			100,
+			33,
+			OrderType::Buy
+		));
+		assert_ok!(Auction::partial_clinch_order(Some(BRUCE).into(), 0, 33));
+		assert_ok!(Auction::revoke_order(Some(ALICE).into(), 0));
+
+		assert_eq!(Auction::order_id(), 1);
+
+		let user_buy_order_ids = Auction::user_order_ids(ALICE, OrderType::Buy);
+		assert_eq!(user_buy_order_ids.len(), 0);
+
+		assert!(Auction::order_info(0).is_none());
+
+		assert_eq!(Tokens::accounts(ALICE, VSBOND).free, 133);
+		assert_eq!(Tokens::accounts(ALICE, VSBOND).frozen, 0);
+		assert_eq!(Tokens::accounts(ALICE, VSBOND).reserved, 0);
+
+		assert_eq!(Tokens::accounts(ALICE, TOKEN).free, 90);
+		assert_eq!(Tokens::accounts(ALICE, TOKEN).frozen, 0);
+		assert_eq!(Tokens::accounts(ALICE, TOKEN).reserved, 0);
+
+		assert_eq!(Tokens::accounts(BRUCE, VSBOND).free, 67);
+		assert_eq!(Tokens::accounts(BRUCE, VSBOND).frozen, 0);
+		assert_eq!(Tokens::accounts(BRUCE, VSBOND).reserved, 0);
+
+		assert_eq!(Tokens::accounts(BRUCE, TOKEN).free, 110);
+		assert_eq!(Tokens::accounts(BRUCE, TOKEN).frozen, 0);
+		assert_eq!(Tokens::accounts(BRUCE, TOKEN).reserved, 0);
 	});
 }
 
@@ -466,9 +508,9 @@ fn partial_clinch_buy_order_should_work() {
 		assert_eq!(Tokens::accounts(ALICE, VSBOND).frozen, 0);
 		assert_eq!(Tokens::accounts(ALICE, VSBOND).reserved, 0);
 
-		assert_eq!(Tokens::accounts(ALICE, TOKEN).free, 67);
+		assert_eq!(Tokens::accounts(ALICE, TOKEN).free, 68);
 		assert_eq!(Tokens::accounts(ALICE, TOKEN).frozen, 0);
-		assert_eq!(Tokens::accounts(ALICE, TOKEN).reserved, 1);
+		assert_eq!(Tokens::accounts(ALICE, TOKEN).reserved, 0);
 
 		assert_eq!(Tokens::accounts(BRUCE, VSBOND).free, 0);
 		assert_eq!(Tokens::accounts(BRUCE, VSBOND).frozen, 0);
@@ -494,7 +536,15 @@ fn partial_clinch_order_not_exist_should_fail() {
 #[test]
 fn clinch_order_by_origin_illegal_should_fail() {
 	new_test_ext().execute_with(|| {
-		assert_ok!(Auction::create_order(Some(ALICE).into(), 3000, 13, 20, 100, 100, OrderType::Sell));
+		assert_ok!(Auction::create_order(
+			Some(ALICE).into(),
+			3000,
+			13,
+			20,
+			100,
+			100,
+			OrderType::Sell
+		));
 		assert_noop!(
 			Auction::clinch_order(Some(ALICE).into(), 0),
 			Error::<Test>::ForbidClinchOrderWithinOwnership
@@ -518,7 +568,15 @@ fn clinch_order_by_origin_illegal_should_fail() {
 #[test]
 fn clinch_order_without_enough_token_should_fail() {
 	new_test_ext().execute_with(|| {
-		assert_ok!(Auction::create_order(Some(ALICE).into(), 3000, 13, 20, 100, 200, OrderType::Sell));
+		assert_ok!(Auction::create_order(
+			Some(ALICE).into(),
+			3000,
+			13,
+			20,
+			100,
+			200,
+			OrderType::Sell
+		));
 		assert_ok!(Auction::partial_clinch_order(Some(BRUCE).into(), 0, 50));
 		assert_noop!(
 			Auction::clinch_order(Some(BRUCE).into(), 0),
