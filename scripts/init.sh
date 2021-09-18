40081bb8940a59b97055b82a1b1aa72df9ebad1f
yrong1997 <yrong1997@gmail.com>
2021-09-18 14:01:49 +0800

    Revamp ci&image flow (#304)
    * Revamp ci&image flow
    
    * Zenlink dependency as forked

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,15 +4,6 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
-if [ -z $CI_PROJECT_NAME ] ; then
-   rustup update nightly
-   rustup update stable
-fi
+rustup default nightly-2021-06-17
 
-rustup target add wasm32-unknown-unknown --toolchain nightly
-
-rustup component add rustfmt
-
-# Install wasm-gc. It's useful for stripping slimming down wasm binaries.
-command -v wasm-gc || \
-	cargo +nightly install --git https://github.com/alexcrichton/wasm-gc --force
+rustup target add wasm32-unknown-unknown --toolchain nightly-2021-06-17
