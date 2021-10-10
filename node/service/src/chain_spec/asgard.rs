5c1387cd05af87592f59c6072da25221d7a7910b
Herry Ho <45537372+herryho@users.noreply.github.com>
2021-10-10 16:21:49 +0800

    unit tests done (#327)
    * unit tests done
    
    * add lightening-redeem benchmarking to asgard runtime
    
    * change LighteningRedeemPalletId

--- node/service/src/chain_spec/asgard.rs
+++ node/service/src/chain_spec/asgard.rs
@@ -217,6 +217,12 @@ fn local_config_genesis(id: ParaId) -> GenesisConfig {
 				(x.clone(), CurrencyId::Stable(TokenSymbol::KUSD), ENDOWMENT * 4_000_000),
 				(x.clone(), CurrencyId::Token(TokenSymbol::KSM), ENDOWMENT * 4_000_000),
 				(x.clone(), CurrencyId::Native(TokenSymbol::ASG), ENDOWMENT * 4_000_000),
+				(x.clone(), CurrencyId::VSToken(TokenSymbol::KSM), ENDOWMENT * 4_000_000),
+				(
+					x.clone(),
+					CurrencyId::VSBond(TokenSymbol::BNC, 2001, 13, 20),
+					ENDOWMENT * 4_000_000,
+				),
 			]
 		})
 		.collect();
