85e01a0be2e6c6ccfff12b9fe8b67bef7e159702
Herry Ho <herry.heyi@gmail.com>
2021-11-09 19:05:29 +0800

    half-way

---
+++ runtime/bifrost-polkadot/src/weights/mod.rs
@@ -0,0 +1,39 @@
+// This file is part of Bifrost.
+
+// Copyright (C) 2019-2021 Liebi Technologies (UK) Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 2.0.0
+
+//! A list of the different weight modules for our runtime.
+
+pub mod bifrost_call_switchgear;
+pub mod bifrost_flexible_fee;
+pub mod bifrost_lightening_redeem;
+pub mod bifrost_liquidity_mining;
+pub mod bifrost_salp;
+pub mod bifrost_salp_lite;
+pub mod bifrost_token_issuer;
+pub mod frame_system;
+pub mod orml_tokens;
+pub mod pallet_balances;
+pub mod pallet_bounties;
+pub mod pallet_indices;
+pub mod pallet_scheduler;
+pub mod pallet_timestamp;
+pub mod pallet_treasury;
+pub mod pallet_utility;
+pub mod pallet_vesting;
