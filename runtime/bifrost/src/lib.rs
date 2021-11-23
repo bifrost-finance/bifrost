6d1a8bb9653b744fe0e66c6ff4a0e49d95ce1ca4
Allen Pocket <AllenPocketGamer@Gmail.com>
2021-11-23 19:51:24 +0800

    :arrow_up: ($RUNTIME) Bump version to v0.9.12

--- runtime/bifrost/src/lib.rs
+++ runtime/bifrost/src/lib.rs
@@ -132,7 +132,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bifrost"),
 	impl_name: create_runtime_str!("bifrost"),
 	authoring_version: 1,
-	spec_version: 910,
+	spec_version: 912,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
