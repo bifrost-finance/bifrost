2184ed5582057fdc43745a810a0e6e57a9f5c9cc
yrong <yrong1997@gmail.com>
2021-12-14 18:46:55 +0800

    Revamp redeem flow in salp-lite

--- pallets/salp-lite/src/benchmarking.rs
+++ pallets/salp-lite/src/benchmarking.rs
@@ -54,24 +54,6 @@ fn contribute_fund<T: Config>(who: &T::AccountId, index: ParaId) {
 
 benchmarks! {
 
-	refund {
-		let fund_index = create_fund::<T>(1);
-		let caller: T::AccountId = whitelisted_caller();
-		let caller_origin: T::Origin = RawOrigin::Signed(caller.clone()).into();
-		let contribution = T::MinContribution::get();
-		contribute_fund::<T>(&caller,fund_index);
-		assert_ok!(Salp::<T>::fund_fail(RawOrigin::Root.into(), fund_index));
-		assert_ok!(Salp::<T>::withdraw(RawOrigin::Root.into(), fund_index));
-		let fund = Salp::<T>::funds(fund_index).unwrap();
-		let (_, status) = Salp::<T>::contribution(fund.trie_index, &caller);
-		assert_eq!(status, ContributionStatus::Idle);
-	}: _(RawOrigin::Signed(caller.clone()), fund_index)
-	verify {
-		let (_, status) = Salp::<T>::contribution(fund.trie_index, &caller);
-		assert_eq!(status, ContributionStatus::Idle);
-		assert_last_event::<T>(Event::<T>::Refunded(caller.clone(), fund_index, contribution).into())
-	}
-
 	redeem {
 		let fund_index = create_fund::<T>(1);
 		let caller: T::AccountId = whitelisted_caller();
