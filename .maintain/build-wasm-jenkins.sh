da970fa22088bb1e18deded2141c768224c617f3
yooml <ymlll0508@gmail.com>
2021-12-17 11:20:21 +0800

    feat: 🎸 add build-bifrost-wasm-jenkins for Makefile

---
+++ .maintain/build-wasm-jenkins.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -xe
+
+RUSTC_VERSION="1.56.1"
+EXTRA_ARGS='--json'
+BIN_PATH=$(dirname $(readlink -f $0))
+WORK_PATH=${BIN_PATH}/..
+
+RUNTIME=$1
+
+docker run --rm -i \
+  -e PACKAGE=$RUNTIME-runtime \
+  -e VERBOSE=1 \
+  -e CARGO_TERM_COLOR=always \
+  -v ${TMPDIR}/cargo:/cargo-home \
+  -v ${WORK_PATH}:/build \
+  paritytech/srtool:${RUSTC_VERSION} build ${EXTRA_ARGS}
+
+mkdir -p ${WORK_PATH}/deploy/wasm
+cp ${WORK_PATH}/runtime/${RUNTIME/bifrost-kusama/bifrost}/target/srtool/release/wbuild/$RUNTIME-runtime/${RUNTIME/-/_}_runtime.compact.compressed.wasm \
+${WORK_PATH}/deploy/wasm
