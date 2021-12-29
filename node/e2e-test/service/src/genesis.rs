a60d5335fa802fb514d44074574369fa5ab77fed
yooml <ymlll0508@gmail.com>
2021-12-29 15:31:09 +0800

    feat: 🎸 add bifrost_kusama_runtime

--- node/e2e-test/service/src/genesis.rs
+++ node/e2e-test/service/src/genesis.rs
@@ -23,7 +23,9 @@ use sp_runtime::traits::Block as BlockT;
 
 /// Returns the initial head data for a parachain ID.
 pub fn initial_head_data(para_id: ParaId) -> HeadData {
-	let spec = Box::new(crate::chain_spec::get_chain_spec(para_id));
+	// let spec = Box::new(crate::chain_spec::get_chain_spec(para_id));
+	#[cfg(any(feature = "with-bifrost-kusama-runtime", feature = "with-bifrost-runtime"))]
+	let spec = Box::new(node_service::chain_spec::bifrost_kusama::local_testnet_config().unwrap());
 	let block: Block = generate_genesis_block(&(spec as Box<_>)).unwrap();
 	let genesis_state = block.header().encode();
 	genesis_state.into()
