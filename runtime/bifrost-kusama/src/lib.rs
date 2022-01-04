71abbe4441d631e01ce9203426eb6b532801fc93
Edwin <lark930@gmail.com>
2022-01-04 17:21:42 +0800

    remove VsbondAuctionOnRuntimeUpgrade (#465)

--- runtime/bifrost-kusama/src/lib.rs
+++ runtime/bifrost-kusama/src/lib.rs
@@ -1710,7 +1710,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPallets,
-	VsbondAuctionOnRuntimeUpgrade<Runtime, ()>,
+	(),
 >;
 
 impl_runtime_apis! {
@@ -1990,52 +1990,6 @@ impl_runtime_apis! {
 		fn execute_block_no_check(block: Block) -> Weight {
 			Executive::execute_block_no_check(block)
 		}
-	}
-}
-
-pub struct VsbondAuctionOnRuntimeUpgrade<T, I>(PhantomData<(T, I)>);
-impl<T: bifrost_vsbond_auction::Config<I>, I: 'static> OnRuntimeUpgrade
-	for VsbondAuctionOnRuntimeUpgrade<T, I>
-{
-	#[cfg(feature = "try-runtime")]
-	fn pre_upgrade() -> Result<(), &'static str> {
-		#[allow(unused_imports)]
-		use frame_support::{migration, Identity};
-		log::info!("Bifrost `pre_upgrade`...");
-
-		// check the on-going order number
-		let order_iter = bifrost_vsbond_auction::TotalOrderInfos::<T, I>::iter();
-		let order_count = order_iter.count();
-		log::info!("Old order count is {:?}", order_count);
-
-		Ok(())
-	}
-
-	fn on_runtime_upgrade() -> Weight {
-		log::info!("Bifrost `on_runtime_upgrade`...");
-
-		let weight = bifrost_vsbond_auction::migration::migrate_orders::<Runtime, _>();
-
-		log::info!("Bifrost `on_runtime_upgrade finished`");
-
-		weight
-
-		// RocksDbWeight::get().writes(1)
-	}
-
-	#[cfg(feature = "try-runtime")]
-	fn post_upgrade() -> Result<(), &'static str> {
-		#[allow(unused_imports)]
-		use frame_support::{migration, Identity};
-		log::info!("Bifrost `post_upgrade`...");
-
-		// check the post-upgrader remaining order number
-		let order_iter = bifrost_vsbond_auction::TotalOrderInfos::<T, I>::iter();
-		let order_count = order_iter.count();
-
-		log::info!("New order count is {:?}", order_count);
-
-		Ok(())
 	}
 }
 
