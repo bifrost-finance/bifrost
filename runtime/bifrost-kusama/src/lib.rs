da322d96837fd3d829b3791b74b19d13bd39e7af
yooml <ymlll0508@gmail.com>
2022-01-18 18:49:16 +0800

    fix: 🐛 fix increment-spec-version

--- runtime/bifrost-kusama/src/lib.rs
+++ runtime/bifrost-kusama/src/lib.rs
@@ -158,7 +158,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bifrost"),
 	impl_name: create_runtime_str!("bifrost"),
 	authoring_version: 1,
-	spec_version: 917,
+	spec_version: 921,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
