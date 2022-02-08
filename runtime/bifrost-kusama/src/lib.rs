edfccf1759ed42a16fc06d684aa3c3e9de5e6a22
ubuntu <ubuntu@localhost.localdomain>
2022-02-08 15:26:01 +0800

    Bump to 0.9.26

--- runtime/bifrost-kusama/src/lib.rs
+++ runtime/bifrost-kusama/src/lib.rs
@@ -138,7 +138,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bifrost"),
 	impl_name: create_runtime_str!("bifrost"),
 	authoring_version: 1,
-	spec_version: 930,
+	spec_version: 926,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
