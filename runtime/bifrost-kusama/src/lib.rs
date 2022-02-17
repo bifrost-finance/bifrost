9869f09f920e5161dc5047b0ea1e836837792a01
yooml <ymlll0508@gmail.com>
2022-02-17 16:59:32 +0800

    feat: 🎸 add pallet vtoken-minting

--- runtime/bifrost-kusama/src/lib.rs
+++ runtime/bifrost-kusama/src/lib.rs
@@ -1729,6 +1729,10 @@ pub type ZenlinkLocationToAccountId = (
 	AccountId32Aliases<AnyNetwork, AccountId>,
 );
 
+impl vtoken_minting::Config for Runtime {
+	type Event = Event;
+}
+
 // Below is the implementation of tokens manipulation functions other than native token.
 pub struct LocalAssetAdaptor<Local>(PhantomData<Local>);
 
@@ -1864,6 +1868,7 @@ construct_runtime! {
 		CallSwitchgear: bifrost_call_switchgear::{Pallet, Storage, Call, Event<T>} = 112,
 		VSBondAuction: bifrost_vsbond_auction::{Pallet, Call, Storage, Event<T>} = 113,
 		Slp: bifrost_slp::{Pallet, Call, Storage, Event<T>} = 114,
+		VtokenMinting: vtoken_minting::{Pallet, Call, Storage, Event<T>} = 115,
 	}
 }
 
