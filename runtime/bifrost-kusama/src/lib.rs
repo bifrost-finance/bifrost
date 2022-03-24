5300b33110060da00f85c504adf0e137690cf65b
Herry Ho <45537372+herryho@users.noreply.github.com>
2022-03-24 21:54:17 +0800

    Add movr (#514)
    * add asset movr
    
    * add moonriver chain id and pallet index

--- runtime/bifrost-kusama/src/lib.rs
+++ runtime/bifrost-kusama/src/lib.rs
@@ -1154,6 +1154,14 @@ parameter_types! {
 		// rmrk:KSM = 10:1
 		ksm_per_second() * 10 / 100 //rmrk currency decimal as 10
 	);
+	pub MovrPerSecond: (AssetId, u128) = (
+		MultiLocation::new(
+			1,
+			X2(Parachain(parachains::moonriver::ID), PalletInstance(parachains::moonriver::PALLET_ID.into()))
+		).into(),
+		// MOVR:KSM = 2.67:1
+		ksm_per_second() * 267 * 10_000 //movr currency decimal as 18
+	);
 }
 
 pub struct ToTreasury;
@@ -1182,6 +1190,7 @@ pub type Trader = (
 	FixedRateOfFungible<PhaPerSecond, ToTreasury>,
 	FixedRateOfFungible<RmrkPerSecond, ToTreasury>,
 	FixedRateOfFungible<RmrkNewPerSecond, ToTreasury>,
+	FixedRateOfFungible<MovrPerSecond, ToTreasury>,
 );
 
 pub struct XcmConfig;
@@ -1329,6 +1338,7 @@ orml_traits::parameter_type_with_key! {
 			&CurrencyId::VSBond(TokenSymbol::DOT, ..) => 1 * cent(PolkadotCurrencyId::get()),
 			&CurrencyId::LPToken(..) => 10 * millicent(NativeCurrencyId::get()),
 			&CurrencyId::Token(TokenSymbol::RMRK) => 1 * micro(CurrencyId::Token(TokenSymbol::RMRK)),
+			&CurrencyId::Token(TokenSymbol::MOVR) => 1 * micro(CurrencyId::Token(TokenSymbol::MOVR)),	// MOVR has a decimals of 10e18
 			_ => Balance::max_value(), // unsupported
 		}
 	};
