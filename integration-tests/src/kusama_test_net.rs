721d61fa004713f6d6cbc510d2528983f6e662cb
Akagi201 <akagi201@gmail.com>
2022-05-17 15:46:45 +0800

    fix: fix integration test error

--- integration-tests/src/kusama_test_net.rs
+++ integration-tests/src/kusama_test_net.rs
@@ -22,7 +22,7 @@ use bifrost_runtime_common::dollar;
 use cumulus_primitives_core::ParaId;
 use frame_support::traits::GenesisBuild;
 use node_primitives::TokenSymbol::KSM;
-use polkadot_primitives::v1::{BlockNumber, MAX_CODE_SIZE, MAX_POV_SIZE};
+use polkadot_primitives::v2::{BlockNumber, MAX_CODE_SIZE, MAX_POV_SIZE};
 use polkadot_runtime_parachains::configuration::HostConfiguration;
 use sp_runtime::traits::AccountIdConversion;
 use xcm_emulator::{decl_test_network, decl_test_parachain, decl_test_relay_chain};
