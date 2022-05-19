0f22d9d33bc6a0e1d39d3c88277dc7d7a5de218f
Edwin <lark930@gmail.com>
2022-05-19 22:00:30 +0800

    [skip ci] Fix release scripts (#592)

--- .maintain/publish-release.sh
+++ .maintain/publish-release.sh
@@ -3,14 +3,16 @@
 set -xe
 
 RUSTC_VERSION="1.60.0"
-EXTRA_ARGS='--json'
-RUNTIME=bifrost
 BIN_PATH=$(dirname $(readlink -f $0))
 WORK_PATH=${BIN_PATH}/../
 
-###### build binary
+# build binary
 make production-release
 
-cp target/release/bifrost ${WORK_PATH}/resources
-chmod +x ${WORK_PATH}/resources/bifrost
-tar cjSf ${WORK_PATH}/resources/bifrost-x86_64-linux-gnu.tar.bz2 ${WORK_PATH}/resources/bifrost
+# pack artifacts
+mkdir -p ${WORK_PATH}/artifacts
+mv ${WORK_PATH}/target/release/bifrost ${WORK_PATH}/artifacts/
+pushd artifacts
+sha256sum bifrost | tee bifrost.sha256
+shasum -c bifrost.sha256
+popd
