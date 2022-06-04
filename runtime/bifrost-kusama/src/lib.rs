5f1cdcf0cc75a9a4f2e9d1b5e72c77c1663d31e1
herryho <herry.heyi@gmail.com>
2022-06-04 19:48:16 +0800

    change runtime version

--- runtime/bifrost-kusama/src/lib.rs
+++ runtime/bifrost-kusama/src/lib.rs
@@ -140,7 +140,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bifrost"),
 	impl_name: create_runtime_str!("bifrost"),
 	authoring_version: 1,
-	spec_version: 942,
+	spec_version: 944,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
