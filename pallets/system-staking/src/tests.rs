3ee3d0d5ad1a4a9cca7ce7914a0cc14d835b9a7a
Akagi201 <akagi201@gmail.com>
2022-07-05 10:08:34 +0800

    feat: add system staking benchmark

--- pallets/system-staking/src/tests.rs
+++ pallets/system-staking/src/tests.rs
@@ -16,7 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 #![cfg(test)]
-
 use crate::{mock::*, *};
 use frame_support::{
 	assert_ok,
