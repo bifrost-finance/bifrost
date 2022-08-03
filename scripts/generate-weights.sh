78dc13da3b716e22c15c368d7b585c94868bc2c9
NingBo Wang <2536935847@qq.com>
2022-08-03 14:59:37 +0800

    Fallback generate-weights.sh

--- scripts/generate-weights.sh
+++ scripts/generate-weights.sh
@@ -6,7 +6,7 @@
 # sh ./script/generate-weights.sh bifrost
 
 # 1. Build all-release which is added with "runtime-benchmarks" feature;
-#make build-all-release-with-bench
+make build-all-release-with-bench
 # 2. Filter the pallets of ${runtime} that should be executed benchmark;
 IFS=', ' read -r -a runtimes <<< $@;
 for runtime in "${runtimes[@]}"
@@ -28,7 +28,7 @@ do
                 --execution=wasm \
                 --wasm-execution=compiled \
                 --heap-pages=4096 \
-                --output="./runtime/${runtime}/src/weights2/${pallet}.rs" \
+                --output="./runtime/${runtime}/src/weights/${pallet}.rs" \
                 --template="./frame-weight-template.hbs";
             fi
         done
