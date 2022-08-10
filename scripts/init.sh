09b9f27d784b9db71d2a1a0cbd4ea3f8db46d7ee
Edwin Wang <lark930@gmail.com>
2022-08-10 19:03:14 +0800

    Bump rust version

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
-rustup default nightly-2022-05-11
+rustup default nightly-2022-07-24
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2022-05-11
+rustup target add wasm32-unknown-unknown --toolchain nightly-2022-07-24
