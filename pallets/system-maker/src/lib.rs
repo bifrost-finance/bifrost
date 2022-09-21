ea02a61c36ba277c4152a74263253e4324167bbe
yooml <ymlll0508@gmail.com>
2022-09-21 19:14:26 +0800

    feat: 🎸 add minimum_redeem

--- pallets/system-maker/src/lib.rs
+++ pallets/system-maker/src/lib.rs
@@ -125,6 +125,7 @@ pub mod pallet {
 		pub vcurrency_id: CurrencyId,
 		pub annualization: u32,
 		pub granularity: BalanceOf,
+		pub minimum_redeem: BalanceOf,
 	}
 
 	#[pallet::hooks]
@@ -142,8 +143,7 @@ pub mod pallet {
 				}
 				let redeem_amount =
 					T::MultiCurrency::free_balance(info.vcurrency_id, &system_maker);
-				if redeem_amount > T::VtokenMintingInterface::get_minimums_redeem(info.vcurrency_id)
-				{
+				if redeem_amount >= info.minimum_redeem {
 					Self::handle_redeem_by_currency_id(&system_maker, &info, redeem_amount);
 				}
 			}
