dc2375e83550f25cf517abf70425aa19ac47f936
yooml <ymlll0508@gmail.com>
2022-10-14 12:06:40 +0800

    feat: 🎸 spec_version

--- runtime/bifrost-polkadot/src/lib.rs
+++ runtime/bifrost-polkadot/src/lib.rs
@@ -138,7 +138,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bifrost_polkadot"),
 	impl_name: create_runtime_str!("bifrost_polkadot"),
 	authoring_version: 0,
-	spec_version: 959,
+	spec_version: 960,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
