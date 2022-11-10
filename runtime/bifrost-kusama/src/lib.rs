d04ce33bf38be6b6265e0fdf43dfdd35eeb97de2
Edwin <lark930@gmail.com>
2022-11-10 17:13:23 +0800

    Bifrost v0.9.64 (#817)
    * remove unused dependencies
    
    * bump version to v0.9.64
    
    * Add FixedRateOfAsset to Trader
    
    * Remove foreign asset (#818)
    
    * feat: 🎸 add pallet fee-share
    
    * feat: 🎸 add execute_distribute
    
    * feat: 🎸 add AutoEra
    
    * fix: 🐛 fix
    
    * fix: 🐛 format
    
    * feat: 🎸 add to runtime
    
    * fix: 🐛 fix
    
    * fix: 🐛 get_token_shares
    
    * feat:upgrade vesting (#771)
    
    * Vifl (#772)
    
    * add remark to token-issuer
    
    * add register_linked_account function
    
    * code finished without unit tests
    
    * revamp slp
    
    * done with vfil
    
    * add cross-in-out pallet to runtime
    
    * fixes
    
    * cross-in-out pallet unit tests
    
    * fix filecoin initialize_delegator
    
    * fix tests for initialize_delegato
    
    * fixes
    
    * benchmark file header
    
    * fixes
    
    * fix token-issuer
    
    * more unitests (#773)
    
    * fix: 🐛 add register_vtoken_metadata
    
    * fix test
    
    * Vfil fix -- add new TimeUnit::Kblock type (#782)
    
    * add TimeUnit::Kblock for vfil
    
    * fix unit tests
    
    * add transfer_to function for vfil agent
    
    * Bump version to v0.9.60
    
    * feat: 🎸 add unlock_by_vsbond
    
    * fix: 🐛 on_initialize_update_ledger
    
    * fix:fix integration tests (#787)
    
    * feat: 🎸 polkadot genesis add AssetRegistry
    
    * fix: 🐛 asset-registry genesis_build
    
    * fix: 🐛 unit test
    
    * bump version to v0.9.62
    
    * Migration to register USDT
    
    * Revert vesting (#802)
    
    * feat:revert vesting
    
    * feat:revert vesting
    
    * fix: 🐛 asset-registry genesis_build
    
    * feat:remove foreign_asset_id storage
    
    * feat:remove foreign_asset_id storage
    
    * feat:remove foreign_asset_id storage
    
    Co-authored-by: yooml <ymlll0508@gmail.com>
    Co-authored-by: Herry Ho <45537372+herryho@users.noreply.github.com>
    Co-authored-by: Edwin Wang <lark930@gmail.com>
    
    * adjust-moonriver-parachain-staking-calls-number (#819)
    
    Co-authored-by: NingBo Wang <2536935847@qq.com>
    Co-authored-by: yooml <ymlll0508@gmail.com>
    Co-authored-by: Herry Ho <45537372+herryho@users.noreply.github.com>

--- runtime/bifrost-kusama/src/lib.rs
+++ runtime/bifrost-kusama/src/lib.rs
@@ -145,7 +145,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bifrost"),
 	impl_name: create_runtime_str!("bifrost"),
 	authoring_version: 1,
-	spec_version: 962,
+	spec_version: 964,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
