22bcb3dca7da2793a4ddf151dc32f1ad3bdeb1ae
Akagi201 <akagi201@gmail.com>
2022-11-18 17:26:44 +0800

    feat: add xcm-action pallet

---
+++ pallets/xcm-action/README.md
@@ -0,0 +1,3 @@
+# xcm-action
+
+A pallet to manage the execution of XCM messages.
---
+++ pallets/xcm-action/src/lib.rs
@@ -0,0 +1,208 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+use cumulus_primitives_core::ParaId;
+use frame_support::{
+	dispatch::DispatchResultWithPostInfo, sp_runtime::SaturatedConversion, traits::Get, PalletId,
+};
+use node_primitives::{CurrencyId, TryConvertFrom, VtokenMintingInterface};
+use orml_traits::{arithmetic::Zero, MultiCurrency, XcmTransfer};
+use xcm::{latest::prelude::*, v1::MultiLocation};
+use xcm_interface::traits::parachains;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use super::*;
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+	use zenlink_protocol::{AssetId, ExportZenlink};
+
+	#[allow(type_alias_bounds)]
+	pub type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+
+	#[allow(type_alias_bounds)]
+	pub type CurrencyIdOf<T> = <<T as Config>::MultiCurrency as MultiCurrency<
+		<T as frame_system::Config>::AccountId,
+	>>::CurrencyId;
+
+	#[allow(type_alias_bounds)]
+	pub type BalanceOf<T> =
+		<<T as Config>::MultiCurrency as MultiCurrency<AccountIdOf<T>>>::Balance;
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		type MultiCurrency: MultiCurrency<AccountIdOf<Self>, CurrencyId = CurrencyId>;
+
+		type DexOperator: ExportZenlink<Self::AccountId>;
+
+		/// The interface to call VtokenMinting module functions.
+		type VtokenMintingInterface: VtokenMintingInterface<
+			AccountIdOf<Self>,
+			CurrencyIdOf<Self>,
+			BalanceOf<Self>,
+		>;
+
+		/// xtokens xcm transfer interface
+		type XcmTransfer: XcmTransfer<AccountIdOf<Self>, BalanceOf<Self>, CurrencyIdOf<Self>>;
+
+		/// ModuleID for creating sub account
+		#[pallet::constant]
+		type PalletId: Get<PalletId>;
+
+		#[pallet::constant]
+		type ParachainId: Get<ParaId>;
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		XcmMinted {
+			receiver: [u8; 20],
+			token_id: CurrencyIdOf<T>,
+			token_amount: BalanceOf<T>,
+		},
+		XcmRedeemed {
+			vtoken_id: CurrencyIdOf<T>,
+			vtoken_amount: BalanceOf<T>,
+		},
+		XcmSwapped {
+			receiver: [u8; 20],
+			amount_in_max: BalanceOf<T>,
+			amount_out: BalanceOf<T>,
+			in_currency_id: CurrencyIdOf<T>,
+			out_currency_id: CurrencyIdOf<T>,
+		},
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		/// Token not found in vtoken minting
+		TokenNotFoundInVtokenMinting,
+		/// Token not found in zenlink
+		TokenNotFoundInZenlink,
+	}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// vtoken-minting mint
+		#[pallet::weight(10_000)]
+		pub fn mint(
+			origin: OriginFor<T>,
+			receiver: [u8; 20],
+			token_id: CurrencyIdOf<T>,
+			token_amount: BalanceOf<T>,
+		) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?; // use the derivative acount from OriginConverter
+			T::VtokenMintingInterface::mint(who.clone(), token_id, token_amount)?;
+			let vtoken_id = T::VtokenMintingInterface::vtoken_id(token_id)
+				.ok_or(Error::<T>::TokenNotFoundInVtokenMinting)?;
+			// success
+			let vtoken_balance = T::MultiCurrency::free_balance(vtoken_id, &who);
+			if vtoken_balance != BalanceOf::<T>::zero() {
+				T::XcmTransfer::transfer(
+					who,
+					vtoken_id,
+					vtoken_balance,
+					MultiLocation {
+						parents: 1,
+						interior: X2(
+							Parachain(parachains::moonriver::ID),
+							Junction::AccountKey20 { network: Any, key: receiver },
+						),
+					},
+					4_000_000_000,
+				)
+				.ok();
+			}
+
+			Self::deposit_event(Event::XcmMinted { receiver, token_id, token_amount });
+
+			Ok(().into())
+		}
+
+		/// vtoken-minting redeem
+		#[pallet::weight(10_000)]
+		pub fn redeem(
+			origin: OriginFor<T>,
+			vtoken_id: CurrencyIdOf<T>,
+			vtoken_amount: BalanceOf<T>,
+		) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?; // use the derivative acount from OriginConverter
+			T::VtokenMintingInterface::redeem(who.clone(), vtoken_id, vtoken_amount)?;
+
+			Self::deposit_event(Event::XcmRedeemed { vtoken_id, vtoken_amount });
+
+			Ok(().into())
+		}
+
+		/// zenlink inner_swap_assets_for_exact_assets
+		#[pallet::weight(10_000)]
+		pub fn swap(
+			origin: OriginFor<T>,
+			receiver: [u8; 20],
+			amount_in_max: BalanceOf<T>,
+			amount_out: BalanceOf<T>,
+			in_currency_id: CurrencyIdOf<T>,
+			out_currency_id: CurrencyIdOf<T>,
+		) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?;
+
+			let in_asset_id: AssetId =
+				AssetId::try_convert_from(in_currency_id, T::ParachainId::get().into())
+					.map_err(|_| Error::<T>::TokenNotFoundInZenlink)?;
+
+			let out_asset_id: AssetId =
+				AssetId::try_convert_from(out_currency_id, T::ParachainId::get().into())
+					.map_err(|_| Error::<T>::TokenNotFoundInZenlink)?;
+
+			let path = vec![in_asset_id, out_asset_id];
+			T::DexOperator::inner_swap_assets_for_exact_assets(
+				&who,
+				amount_out.saturated_into(),
+				amount_in_max.saturated_into(),
+				&path,
+				&who,
+			)?;
+
+			let out_balance = T::MultiCurrency::free_balance(out_currency_id, &who);
+			if out_balance != BalanceOf::<T>::zero() {
+				T::XcmTransfer::transfer(
+					who,
+					out_currency_id,
+					out_balance,
+					MultiLocation {
+						parents: 1,
+						interior: X2(
+							Parachain(parachains::moonriver::ID),
+							Junction::AccountKey20 { network: Any, key: receiver },
+						),
+					},
+					4_000_000_000,
+				)
+				.ok();
+			}
+
+			Self::deposit_event(Event::XcmSwapped {
+				receiver,
+				amount_in_max,
+				amount_out,
+				in_currency_id,
+				out_currency_id,
+			});
+
+			Ok(().into())
+		}
+	}
+}
