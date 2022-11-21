085aee28f31ed6eec562d04c26af24f9dffd6b17
NingBo Wang <2536935847@qq.com>
2022-11-21 13:12:52 +0800

    fix:fix token_id value

--- runtime/bifrost-kusama/src/migrations.rs
+++ runtime/bifrost-kusama/src/migrations.rs
@@ -18,120 +18,32 @@
 
 //! A set of constant values used in Bifrost runtime.
 
-use bifrost_asset_registry::{AssetMetadatas, Config, CurrencyIdToLocations, NextForeignAssetId};
+use bifrost_asset_registry::*;
 use frame_support::{pallet_prelude::Weight, traits::Get};
-use node_primitives::{AssetIds, CurrencyId};
 
 pub struct AssetRegistryMigration<T>(sp_std::marker::PhantomData<T>);
 impl<T: Config> frame_support::traits::OnRuntimeUpgrade for AssetRegistryMigration<T> {
+	#[cfg(feature = "try-runtime")]
+	fn pre_upgrade() -> Result<(), &'static str> {
+		assert!(NextTokenId::<T>::get() == 0u8, "NextTokenId == 0");
+
+		log::info!("try-runtime::pre_upgrade NextTokenId value: {:?}", NextTokenId::<T>::get());
+		Ok(())
+	}
+
 	fn on_runtime_upgrade() -> frame_support::weights::Weight {
 		let mut len = Weight::default();
-
-		NextForeignAssetId::<T>::kill();
-		for id in 0..4u32 {
-			AssetMetadatas::<T>::remove(AssetIds::ForeignAssetId(id));
-			CurrencyIdToLocations::<T>::remove(CurrencyId::ForeignAsset(id));
-			len += 2
-		}
-
+		let result = bifrost_asset_registry::Pallet::<T>::get_next_token_id();
+		log::info!("try-runtime:: NextTokenId++ result: {:?}", result);
+		len += 1;
 		<T as frame_system::Config>::DbWeight::get().reads_writes(len, len)
 	}
 
 	#[cfg(feature = "try-runtime")]
-	fn pre_upgrade() -> Result<(), &'static str> {
-		assert!(NextForeignAssetId::<T>::get() == 4, "NextForeignAssetId == 4");
-
-		assert!(
-			AssetMetadatas::<T>::get(AssetIds::ForeignAssetId(0)).is_some(),
-			"ForeignAssetId(0) not exist"
-		);
-		assert!(
-			AssetMetadatas::<T>::get(AssetIds::ForeignAssetId(1)).is_some(),
-			"ForeignAssetId(1) not exist"
-		);
-		assert!(
-			AssetMetadatas::<T>::get(AssetIds::ForeignAssetId(2)).is_some(),
-			"ForeignAssetId(2) not exist"
-		);
-		assert!(
-			AssetMetadatas::<T>::get(AssetIds::ForeignAssetId(3)).is_some(),
-			"ForeignAssetId(3) not exist"
-		);
-
-		assert!(
-			CurrencyIdToLocations::<T>::get(CurrencyId::ForeignAsset(1)).is_some(),
-			"ForeignAssetId(1) not exist"
-		);
-		assert!(
-			CurrencyIdToLocations::<T>::get(CurrencyId::ForeignAsset(2)).is_some(),
-			"ForeignAssetId(2) not exist"
-		);
-		assert!(
-			CurrencyIdToLocations::<T>::get(CurrencyId::ForeignAsset(3)).is_some(),
-			"ForeignAssetId(3) not exist"
-		);
-
-		log::info!(
-			"try-runtime::pre_upgrade NextForeignAssetId value: {:?}",
-			NextForeignAssetId::<T>::get()
-		);
-		log::info!(
-			"try-runtime::pre_upgrade AssetMetadatas ForeignAssetId(0): {:?}",
-			AssetMetadatas::<T>::get(AssetIds::ForeignAssetId(0)).is_some()
-		);
-		log::info!(
-			"try-runtime::pre_upgrade CurrencyIdToLocations ForeignAssetId(0): {:?}",
-			CurrencyIdToLocations::<T>::get(CurrencyId::ForeignAsset(0)).is_some()
-		);
-		Ok(())
-	}
-
-	#[cfg(feature = "try-runtime")]
 	fn post_upgrade() -> Result<(), &'static str> {
-		assert!(NextForeignAssetId::<T>::get() == 0, "NextForeignAssetId == 0");
+		assert!(NextTokenId::<T>::get() == 1u8, "NextTokenId == 1");
 
-		assert!(
-			AssetMetadatas::<T>::get(AssetIds::ForeignAssetId(0)).is_none(),
-			"ForeignAssetId(0) exist"
-		);
-		assert!(
-			AssetMetadatas::<T>::get(AssetIds::ForeignAssetId(1)).is_none(),
-			"ForeignAssetId(1) exist"
-		);
-		assert!(
-			AssetMetadatas::<T>::get(AssetIds::ForeignAssetId(2)).is_none(),
-			"ForeignAssetId(2) exist"
-		);
-		assert!(
-			AssetMetadatas::<T>::get(AssetIds::ForeignAssetId(3)).is_none(),
-			"ForeignAssetId(3) exist"
-		);
-
-		assert!(
-			CurrencyIdToLocations::<T>::get(CurrencyId::ForeignAsset(1)).is_none(),
-			"ForeignAssetId(1) exist"
-		);
-		assert!(
-			CurrencyIdToLocations::<T>::get(CurrencyId::ForeignAsset(2)).is_none(),
-			"ForeignAssetId(2) exist"
-		);
-		assert!(
-			CurrencyIdToLocations::<T>::get(CurrencyId::ForeignAsset(3)).is_none(),
-			"ForeignAssetId(3) exist"
-		);
-
-		log::info!(
-			"try-runtime::post_upgrade NextForeignAssetId value: {:?}",
-			NextForeignAssetId::<T>::get()
-		);
-		log::info!(
-			"try-runtime::post_upgrade AssetMetadatas ForeignAssetId(0): {:?}",
-			AssetMetadatas::<T>::get(AssetIds::ForeignAssetId(0)).is_some()
-		);
-		log::info!(
-			"try-runtime::post_upgrade CurrencyIdToLocations ForeignAssetId(0): {:?}",
-			CurrencyIdToLocations::<T>::get(CurrencyId::ForeignAsset(0)).is_some()
-		);
+		log::info!("try-runtime::pre_upgrade NextTokenId value: {:?}", NextTokenId::<T>::get());
 		Ok(())
 	}
 }
