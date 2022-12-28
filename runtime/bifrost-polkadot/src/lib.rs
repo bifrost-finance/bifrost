8ba865a266298a16902200c836ac81b3aebd7c97
Edwin <lark930@gmail.com>
2022-12-28 11:44:18 +0800

    Update build wasm (#863)
    * Bump version to v0.9.68
    
    * [skip ci]Update RUSTC_VERSION to 1.64.0

--- runtime/bifrost-polkadot/src/lib.rs
+++ runtime/bifrost-polkadot/src/lib.rs
@@ -142,7 +142,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bifrost_polkadot"),
 	impl_name: create_runtime_str!("bifrost_polkadot"),
 	authoring_version: 0,
-	spec_version: 966,
+	spec_version: 968,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
