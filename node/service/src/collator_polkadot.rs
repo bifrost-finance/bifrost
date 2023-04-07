8d41ce34d0198c216e0787e21530116d2625c8c5
NingBo Wang <2536935847@qq.com>
2023-04-07 08:12:30 +0800

    Bump version (#948)

--- node/service/src/collator_polkadot.rs
+++ node/service/src/collator_polkadot.rs
@@ -33,7 +33,7 @@ use cumulus_client_service::{
 	StartCollatorParams, StartFullNodeParams,
 };
 use cumulus_primitives_core::ParaId;
-use cumulus_relay_chain_interface::{RelayChainError, RelayChainInterface};
+use cumulus_relay_chain_interface::RelayChainInterface;
 use node_primitives::{Block, Hash};
 use sc_consensus::{ImportQueue, LongestChain};
 use sc_executor::WasmExecutor;
@@ -301,10 +301,8 @@ async fn start_node_impl(
 		hwbench.clone(),
 	)
 	.await
-	.map_err(|e| match e {
-		RelayChainError::ServiceError(polkadot_service::Error::Sub(x)) => x,
-		s => s.to_string().into(),
-	})?;
+	.map_err(|e| sc_service::Error::Application(Box::new(e) as Box<_>))?;
+
 	let block_announce_validator = BlockAnnounceValidator::new(relay_chain_interface.clone(), id);
 	let force_authoring = parachain_config.force_authoring;
 	let validator = parachain_config.role.is_authority();
