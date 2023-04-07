b6d0f0b7c04e6eda747df9a7068977295e2e45e3
herryho <herry.heyi@gmail.com>
2023-04-07 12:04:53 +0800

    fix-vglmr-remark-event

--- pallets/slp/src/agents/moonbeam_agent/types.rs
+++ pallets/slp/src/agents/moonbeam_agent/types.rs
@@ -87,9 +87,7 @@ pub enum MoonbeamXtokensCall<T: Config> {
 #[derive(Encode, Decode, RuntimeDebug, Clone)]
 pub enum MoonbeamSystemCall {
 	#[codec(index = 7)]
-	MoonriverRemarkWithEvent(Box<Vec<u8>>),
-	#[codec(index = 8)]
-	MoonbeamRemarkWithEvent(Box<Vec<u8>>),
+	RemarkWithEvent(Box<Vec<u8>>),
 }
 
 #[derive(PartialEq, Eq, Copy, Clone, Encode, Decode, RuntimeDebug, TypeInfo)]
