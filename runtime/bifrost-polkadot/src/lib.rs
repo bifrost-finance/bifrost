96a50d56ae1618c689ba03db1c94c903eef88a4c
herryho <herry.heyi@gmail.com>
2023-04-20 00:46:25 +0800

    change version to v0.9.74

--- runtime/bifrost-polkadot/src/lib.rs
+++ runtime/bifrost-polkadot/src/lib.rs
@@ -126,7 +126,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bifrost_polkadot"),
 	impl_name: create_runtime_str!("bifrost_polkadot"),
 	authoring_version: 0,
-	spec_version: 972,
+	spec_version: 974,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
