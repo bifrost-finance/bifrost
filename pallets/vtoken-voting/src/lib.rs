2104899734d1d4bb35b629a295398b7372972dcb
Edwin Wang <lark930@gmail.com>
2023-09-07 21:21:46 +0800

    Fix benchmark

--- pallets/vtoken-voting/src/lib.rs
+++ pallets/vtoken-voting/src/lib.rs
@@ -290,7 +290,7 @@ pub mod pallet {
 		_,
 		Twox64Concat,
 		BlockNumberFor<T>,
-		BoundedVec<(CurrencyIdOf<T>, PollIndex), ConstU32<50>>,
+		BoundedVec<(CurrencyIdOf<T>, PollIndex), ConstU32<256>>,
 		ValueQuery,
 	>;
 
@@ -351,7 +351,7 @@ pub mod pallet {
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		#[pallet::call_index(0)]
-		#[pallet::weight(<T as Config>::WeightInfo::vote_new())]
+		#[pallet::weight(<T as Config>::WeightInfo::vote_new().max(<T as Config>::WeightInfo::vote_existing()))]
 		pub fn vote(
 			origin: OriginFor<T>,
 			vtoken: CurrencyIdOf<T>,
