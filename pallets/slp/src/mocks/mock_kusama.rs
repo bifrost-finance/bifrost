0ac46f74a173058dcf833efb7b14498c630fdb3a
herryho <herry.heyi@gmail.com>
2023-10-30 12:06:19 +0800

    fix slp benchmark

--- pallets/slp/src/mocks/mock_kusama.rs
+++ pallets/slp/src/mocks/mock_kusama.rs
@@ -206,7 +206,7 @@ impl orml_xtokens::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type Balance = Balance;
 	type CurrencyId = CurrencyId;
-	type CurrencyIdConvert = ();
+	type CurrencyIdConvert = BifrostCurrencyIdConvert;
 	type AccountIdToMultiLocation = ();
 	type UniversalLocation = UniversalLocation;
 	type SelfLocation = SelfRelativeLocation;
