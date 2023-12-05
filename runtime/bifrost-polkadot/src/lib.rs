da311d6702dfa862a03871cb00ec5f134a65a4eb
Edwin Wang <lark930@gmail.com>
2023-12-05 16:16:40 +0800

    Bifrost v0.9.90

--- runtime/bifrost-polkadot/src/lib.rs
+++ runtime/bifrost-polkadot/src/lib.rs
@@ -131,7 +131,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bifrost_polkadot"),
 	impl_name: create_runtime_str!("bifrost_polkadot"),
 	authoring_version: 0,
-	spec_version: 987,
+	spec_version: 990,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
