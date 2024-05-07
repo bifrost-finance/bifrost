b4cb114632d1b2d874033700e570afe097d154a7
Herry Ho <45537372+herryho@users.noreply.github.com>
2024-05-07 11:39:13 +0800

    clouds-convert pallet (#1225)
    * clouds-convert pallet
    
    * fix clippy
    
    * fix test

--- runtime/bifrost-polkadot/src/weights/mod.rs
+++ runtime/bifrost-polkadot/src/weights/mod.rs
@@ -23,6 +23,7 @@
 pub mod bifrost_asset_registry;
 pub mod bifrost_call_switchgear;
 pub mod bifrost_channel_commission;
+pub mod bifrost_clouds_convert;
 pub mod bifrost_cross_in_out;
 pub mod bifrost_currencies;
 pub mod bifrost_farming;
