410b784c82ac566a1cdbd1708b919147b2f16391
Edwin Wang <lark930@gmail.com>
2024-06-16 17:51:34 +0800

    Fix `encode_evm_address` and `decode_evm_address`

--- runtime/bifrost-kusama/src/evm/precompiles/erc20_mapping.rs
+++ runtime/bifrost-kusama/src/evm/precompiles/erc20_mapping.rs
@@ -21,15 +21,16 @@
 // compliance with the License.                                          http://www.apache.org/licenses/LICENSE-2.0
 
 use crate::evm::precompiles::EvmAddress;
-use bifrost_primitives::AssetId;
+use bifrost_primitives::CurrencyId;
 use hex_literal::hex;
+use parity_scale_codec::{Decode, Encode};
 use primitive_types::H160;
 
 /// A mapping between AssetId and Erc20 EVM address.
 pub trait Erc20Mapping {
-	fn encode_evm_address(asset_id: AssetId) -> Option<EvmAddress>;
+	fn encode_evm_address(asset_id: CurrencyId) -> Option<EvmAddress>;
 
-	fn decode_evm_address(evm_address: EvmAddress) -> Option<AssetId>;
+	fn decode_evm_address(evm_address: EvmAddress) -> Option<CurrencyId>;
 }
 
 pub struct HydraErc20Mapping;
@@ -37,37 +38,15 @@ pub struct HydraErc20Mapping;
 /// Erc20Mapping logic for HydraDX
 /// The asset id (with type u32) is encoded in the last 4 bytes of EVM address
 impl Erc20Mapping for HydraErc20Mapping {
-	fn encode_evm_address(asset_id: AssetId) -> Option<EvmAddress> {
-		let asset_id_bytes: [u8; 4] = asset_id.to_le_bytes();
-
-		let mut evm_address_bytes = [0u8; 20];
-
-		evm_address_bytes[15] = 1;
-
-		for i in 0..4 {
-			evm_address_bytes[16 + i] = asset_id_bytes[3 - i];
-		}
-
-		Some(EvmAddress::from(evm_address_bytes))
+	fn encode_evm_address(asset_id: CurrencyId) -> Option<EvmAddress> {
+		asset_id.encode().try_into().ok().map(|bytes: [u8; 20]| EvmAddress::from(bytes))
 	}
 
-	fn decode_evm_address(evm_address: EvmAddress) -> Option<AssetId> {
-		if !is_asset_address(evm_address) {
-			return None;
-		}
-
-		let mut asset_id: u32 = 0;
-		for byte in evm_address.as_bytes() {
-			asset_id = (asset_id << 8) | (*byte as u32);
-		}
-
-		Some(asset_id)
+	fn decode_evm_address(evm_address: EvmAddress) -> Option<CurrencyId> {
+		CurrencyId::decode(&mut evm_address.as_bytes()).ok()
 	}
 }
 
 pub fn is_asset_address(address: H160) -> bool {
-	let asset_address_prefix =
-		&(H160::from(hex!("0000000000000000000000000000000100000000"))[0..16]);
-
-	&address.to_fixed_bytes()[0..16] == asset_address_prefix
+	CurrencyId::decode(&mut address.as_bytes()).is_ok()
 }
