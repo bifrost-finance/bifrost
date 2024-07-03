90540c51661cabe5b620ead59f709f2f44dcd9ca
yooml <ymlll0508@gmail.com>
2024-07-03 22:52:53 +0800

    fix: 🐛 update sp-api

--- runtime/bifrost-polkadot/build.rs
+++ runtime/bifrost-polkadot/build.rs
@@ -17,21 +17,14 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 #[cfg(all(feature = "metadata-hash", feature = "std"))]
 fn main() {
-	substrate_wasm_builder::WasmBuilder::new()
-		.with_current_project()
-		.export_heap_base()
-		.import_memory()
+	substrate_wasm_builder::WasmBuilder::init_with_defaults()
 		.enable_metadata_hash("BNC", 12)
 		.build()
 }
 
 #[cfg(all(not(feature = "metadata-hash"), feature = "std"))]
 fn main() {
-	substrate_wasm_builder::WasmBuilder::new()
-		.with_current_project()
-		.export_heap_base()
-		.import_memory()
-		.build()
+	substrate_wasm_builder::WasmBuilder::build_using_defaults()
 }
 
 #[cfg(not(feature = "std"))]
